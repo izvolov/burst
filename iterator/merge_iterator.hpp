34d9dde9dd49bd7098e0b21b3ab040deb468b0ab
Дмитрий Изволов <dmitriy@izvolov.ru>
2014-11-18 23:10:20 +0300

    Итератор слияния.

---
+++ iterator/merge_iterator.hpp
@@ -0,0 +1,152 @@
+#ifndef THRUST_ITERATOR_MERGE_ITERATOR_HPP
+#define THRUST_ITERATOR_MERGE_ITERATOR_HPP
+
+#include <algorithm>
+#include <vector>
+
+#include <boost/algorithm/cxx11/all_of.hpp>
+#include <boost/algorithm/cxx11/copy_if.hpp>
+#include <boost/assert.hpp>
+#include <boost/bind.hpp>
+#include <boost/range.hpp>
+#include <boost/range/algorithm_ext/is_sorted.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_same.hpp>
+
+namespace thrust
+{
+    template <typename Compare>
+    struct front_value_comparator
+    {
+        front_value_comparator (const Compare & compare):
+            compare(compare)
+        {
+        }
+
+        template <typename Range>
+        bool operator () (const Range & left, const Range & right) const
+        {
+            return compare(left.front(), right.front());
+        }
+
+        Compare compare;
+    };
+
+    template <typename Compare>
+    struct invert_comparison
+    {
+        invert_comparison (const Compare & compare):
+            compare(compare)
+        {
+        }
+
+        template <typename Object>
+        bool operator () (const Object & left, const Object & right) const
+        {
+            return compare(right, left);
+        }
+
+        Compare compare;
+    };
+
+    template <typename InputRange, typename Compare>
+    class merge_iterator: public boost::iterator_facade
+                                 <
+                                     merge_iterator<InputRange, Compare>,
+                                     typename InputRange::value_type,
+                                     boost::forward_traversal_tag,
+                                     typename InputRange::reference
+                                 >
+    {
+    private:
+        typedef InputRange range_type;
+
+        typedef boost::iterator_facade
+        <
+            merge_iterator,
+            typename range_type::value_type,
+            boost::forward_traversal_tag,
+            typename range_type::reference
+        >
+        base_type;
+
+    public:
+        template <typename InputRange1>
+        merge_iterator (const InputRange1 & ranges, Compare compare):
+            m_range_heap(),
+            m_heap_order(compare)
+        {
+            BOOST_STATIC_ASSERT(boost::is_same<typename InputRange1::value_type, range_type>::value);
+            BOOST_ASSERT(boost::algorithm::all_of(m_range_heap.begin(), m_range_heap.end(), boost::bind(&boost::is_sorted<range_type, Compare>, _1, compare)));
+
+            m_range_heap.reserve(ranges.size());
+            boost::algorithm::copy_if(ranges, std::back_inserter(m_range_heap), not boost::bind(&range_type::empty, _1));
+            std::make_heap(m_range_heap.begin(), m_range_heap.end(), m_heap_order);
+        }
+
+        merge_iterator ():
+            m_range_heap(),
+            m_heap_order(Compare())
+        {
+        }
+
+    public:
+        void increment ()
+        {
+            std::pop_heap(m_range_heap.begin(), m_range_heap.end(), m_heap_order);
+            range_type & range = m_range_heap.back();
+
+            range.advance_begin(1);
+            if (not range.empty())
+            {
+                std::push_heap(m_range_heap.begin(), m_range_heap.end(), m_heap_order);
+            }
+            else
+            {
+                m_range_heap.pop_back();
+            }
+        }
+
+    public:
+        typename base_type::reference dereference () const
+        {
+            return m_range_heap.front().front();
+        }
+
+        bool equal (const merge_iterator & that) const
+        {
+            return this->m_range_heap == that.m_range_heap;
+        }
+
+    private:
+        std::vector<range_type> m_range_heap;
+
+        // invert_comparison устраняет путаницу с обратным порядком в пирамиде при работе с std::make(push, pop)_heap.
+        typedef front_value_comparator<invert_comparison<Compare>> heap_order_type;
+        heap_order_type m_heap_order;
+    };
+
+    template <typename RangeRange, typename Compare>
+    boost::iterator_range
+    <
+        merge_iterator<typename RangeRange::value_type, Compare>
+    >
+    merge (const RangeRange & ranges, Compare compare)
+    {
+        typedef merge_iterator<typename RangeRange::value_type, Compare> iterator;
+        return boost::make_iterator_range(iterator(ranges, compare), iterator());
+    }
+
+    template <typename RangeRange>
+    boost::iterator_range
+    <
+        merge_iterator<typename RangeRange::value_type, std::less<>>
+    >
+    merge (const RangeRange & ranges)
+    {
+        typedef merge_iterator<typename RangeRange::value_type, std::less<>> iterator;
+        return boost::make_iterator_range(iterator(ranges, std::less<>()), iterator());
+    }
+} // namespace thrust
+
+#endif // THRUST_ITERATOR_MERGE_ITERATOR_HPP
---
+++ tests/merge_iterator.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <list>
+
+#include <boost/bind.hpp>
+#include <boost/range.hpp>
+#include <boost/range/algorithm/for_each.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <iterator/merge_iterator.hpp>
+
+BOOST_AUTO_TEST_SUITE(merge_iterator)
+    BOOST_AUTO_TEST_CASE(merging_empty_ranges_results_empty_range)
+    {
+        std::vector<int> first;
+        std::vector<int> second;
+        auto ranges = {boost::make_iterator_range(first), boost::make_iterator_range(second)};
+
+        auto merged_range = thrust::merge(ranges);
+        BOOST_CHECK(merged_range.empty());
+    }
+
+    BOOST_AUTO_TEST_CASE(merging_one_range_results_the_same_range)
+    {
+        int array[10] = {1, 2, 3, 4};
+        auto ranges = {boost::make_iterator_range(array)};
+
+        auto merged_range = thrust::merge(ranges);
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::begin(array), boost::end(array),
+            boost::begin(merged_range), boost::end(merged_range)
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(merging_two_intersecting_sorted_ranges_results_correct_sorted_range)
+    {
+        std::list<char> first{'h', 'f', 'd', 'b'};
+        std::list<char> second{'g', 'e', 'c', 'a'};
+        auto ranges = {boost::make_iterator_range(first), boost::make_iterator_range(second)};
+
+        auto merged_range = thrust::merge(ranges, std::greater<>());
+
+        std::vector<char> expected_collection{'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::begin(merged_range), boost::end(merged_range),
+            boost::begin(expected_collection), boost::end(expected_collection)
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(merging_several_disjoint_sorted_ranges_results_correct_sorted_range)
+    {
+        auto first = {1, 2, 3};
+        auto second = {4, 5, 6};
+        auto third = {7, 8, 9};
+        auto ranges =
+        {
+            boost::make_iterator_range(first),
+            boost::make_iterator_range(second),
+            boost::make_iterator_range(third)
+        };
+
+        auto merged_range = thrust::merge(ranges, std::less<>());
+
+        auto expected_collection = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::begin(merged_range), boost::end(merged_range),
+            boost::begin(expected_collection), boost::end(expected_collection)
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(modifying_merged_mutable_ranges_is_allowed)
+    {
+        std::vector<int> first{100, 50};
+        std::vector<int> second{70, 30};
+
+        auto ranges = {boost::make_iterator_range(first), boost::make_iterator_range(second)};
+
+        auto merged_range = thrust::merge(ranges, std::greater<>());
+        boost::for_each(merged_range, [] (auto & x) { x /= 10; });
+
+        BOOST_CHECK_EQUAL(first[0], 10);
+        BOOST_CHECK_EQUAL(first[1], 5);
+        BOOST_CHECK_EQUAL(second[0], 7);
+        BOOST_CHECK_EQUAL(second[1], 3);
+    }
+BOOST_AUTO_TEST_SUITE_END()
