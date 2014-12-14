8474c704ddb276f78213c641af8f997a5ba57166
Дмитрий Изволов <dmitriy@izvolov.ru>
2014-12-14 23:39:03 +0300

    refs #14 Улучшение форматирования.

--- test/algorithm/sorting/counting_sort.cpp
+++ test/algorithm/sorting/counting_sort.cpp
@@ -9,35 +9,33 @@
 #include <thrust/algorithm/sorting/counting_sort.hpp>
 
 BOOST_AUTO_TEST_SUITE(counting_sort)
+    BOOST_AUTO_TEST_CASE(sorting_empty_range_does_nothing)
+    {
+        std::vector<unsigned char> values;
 
-BOOST_AUTO_TEST_CASE(sorting_empty_range_does_nothing)
-{
-    std::vector<unsigned char> values;
+        std::vector<unsigned char> sorted_values(values.size());
+        thrust::counting_sort(values.begin(), values.end(), sorted_values.begin());
 
-    std::vector<unsigned char> sorted_values(values.size());
-    thrust::counting_sort(values.begin(), values.end(), sorted_values.begin());
+        BOOST_CHECK(values == sorted_values);
+    }
 
-    BOOST_CHECK(values == sorted_values);
-}
+    BOOST_AUTO_TEST_CASE(sorting_descending_range_results_ascending_range)
+    {
+        std::vector<std::size_t> descending_by_low_byte{0x0104, 0x0203, 0x0302, 0x0401};
 
-BOOST_AUTO_TEST_CASE(sorting_descending_range_results_ascending_range)
-{
-    std::vector<std::size_t> descending_by_low_byte{0x0104, 0x0203, 0x0302, 0x0401};
+        std::vector<std::size_t> ascending_by_low_byte(descending_by_low_byte.size());
+        thrust::counting_sort
+        (
+            descending_by_low_byte.begin(),
+            descending_by_low_byte.end(),
+            ascending_by_low_byte.begin(),
+            [] (const std::size_t & integer) -> unsigned char { return integer & 0xff; }
+        );
 
-    std::vector<std::size_t> ascending_by_low_byte(descending_by_low_byte.size());
-    thrust::counting_sort
-    (
-        descending_by_low_byte.begin(),
-        descending_by_low_byte.end(),
-        ascending_by_low_byte.begin(),
-        [] (const std::size_t & integer) -> unsigned char { return integer & 0xff; }
-    );
-
-    BOOST_CHECK_EQUAL_COLLECTIONS
-    (
-        boost::rbegin(descending_by_low_byte), boost::rend(descending_by_low_byte),
-        boost::begin(ascending_by_low_byte), boost::end(ascending_by_low_byte)
-    );
-}
-
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::rbegin(descending_by_low_byte), boost::rend(descending_by_low_byte),
+            boost::begin(ascending_by_low_byte), boost::end(ascending_by_low_byte)
+        );
+    }
 BOOST_AUTO_TEST_SUITE_END()
