8bdc7dc39da147371c749c946e75dc3ea7512994
Дмитрий Изволов <dmitriy@izvolov.ru>
2014-12-15 15:29:52 +0300

    Улучшение форматирования тестов.

--- test/algorithm/searching/bitap.cpp
+++ test/algorithm/searching/bitap.cpp
@@ -8,100 +8,98 @@
 #include <thrust/algorithm/searching/bitap.hpp>
 
 BOOST_AUTO_TEST_SUITE(Bitap)
+    BOOST_AUTO_TEST_CASE(pattern_is_found_when_equal_to_whole_text)
+    {
+        std::string pattern("Привет!");
+        std::string text(pattern);
+        thrust::algorithm::bitap<char, std::uint16_t> search(pattern);
 
-BOOST_AUTO_TEST_CASE(pattern_is_found_when_equal_to_whole_text)
-{
-    std::string pattern("Привет!");
-    std::string text(pattern);
-    thrust::algorithm::bitap<char, std::uint16_t> search(pattern);
+        auto match_position = search(text.begin(), text.end());
 
-    auto match_position = search(text.begin(), text.end());
+        BOOST_CHECK(match_position == text.begin());
+    }
 
-    BOOST_CHECK(match_position == text.begin());
-}
+    BOOST_AUTO_TEST_CASE(pattern_is_found_at_the_first_match_in_a_text_having_several_matches)
+    {
+        std::string pattern("абырвалг");
+        std::string text(pattern + "абырвалгович");
+        thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
 
-BOOST_AUTO_TEST_CASE(pattern_is_found_at_the_first_match_in_a_text_having_several_matches)
-{
-    std::string pattern("абырвалг");
-    std::string text(pattern + "абырвалгович");
-    thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
+        auto match_position = search(text.begin(), text.end());
 
-    auto match_position = search(text.begin(), text.end());
+        BOOST_CHECK(match_position == text.begin());
+    }
 
-    BOOST_CHECK(match_position == text.begin());
-}
+    BOOST_AUTO_TEST_CASE(pattern_is_found_at_the_end_of_a_text)
+    {
+        std::string pattern("ойойой");
+        std::string text("FFUUU" + pattern);
+        thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
 
-BOOST_AUTO_TEST_CASE(pattern_is_found_at_the_end_of_a_text)
-{
-    std::string pattern("ойойой");
-    std::string text("FFUUU" + pattern);
-    thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
+        auto match_position = search(text.begin(), text.end());
 
-    auto match_position = search(text.begin(), text.end());
+        typedef typename std::iterator_traits<std::string::iterator>::difference_type difference_type;
+        difference_type match_offset = static_cast<difference_type>(text.find_first_not_of("FU"));
+        BOOST_CHECK(match_position == text.begin() + match_offset);
+    }
 
-    typedef typename std::iterator_traits<std::string::iterator>::difference_type difference_type;
-    difference_type match_offset = static_cast<difference_type>(text.find_first_not_of("FU"));
-    BOOST_CHECK(match_position == text.begin() + match_offset);
-}
+    BOOST_AUTO_TEST_CASE(pattern_is_not_found_in_a_text_with_no_matches)
+    {
+        std::string pattern("123");
+        std::string text("12131415");
+        thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
 
-BOOST_AUTO_TEST_CASE(pattern_is_not_found_in_a_text_with_no_matches)
-{
-    std::string pattern("123");
-    std::string text("12131415");
-    thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
+        auto match_position = search(text.begin(), text.end());
 
-    auto match_position = search(text.begin(), text.end());
+        BOOST_CHECK(match_position == text.end());
+    }
 
-    BOOST_CHECK(match_position == text.end());
-}
+    BOOST_AUTO_TEST_CASE(pattern_is_found_at_the_next_match_when_search_started_in_the_middle_of_the_first_match)
+    {
+        std::string pattern("asdf");
+        std::string text(pattern + pattern);
+        thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
 
-BOOST_AUTO_TEST_CASE(pattern_is_found_at_the_next_match_when_search_started_in_the_middle_of_the_first_match)
-{
-    std::string pattern("asdf");
-    std::string text(pattern + pattern);
-    thrust::algorithm::bitap<char, std::bitset<32>> search(pattern);
+        auto match_position = search(text.begin() + 1, text.end());
 
-    auto match_position = search(text.begin() + 1, text.end());
+        typedef typename std::iterator_traits<std::string::iterator>::difference_type difference_type;
+        difference_type repeat_offset = static_cast<difference_type>(pattern.size());
+        BOOST_CHECK(match_position == text.begin() + repeat_offset);
+    }
 
-    typedef typename std::iterator_traits<std::string::iterator>::difference_type difference_type;
-    difference_type repeat_offset = static_cast<difference_type>(pattern.size());
-    BOOST_CHECK(match_position == text.begin() + repeat_offset);
-}
+    BOOST_AUTO_TEST_CASE(first_of_two_overlapping_patterns_is_found)
+    {
+        std::string pattern("шалаш");
+        std::string text("   шалашалаш   ");
+        thrust::algorithm::bitap<char, std::bitset<32>, std::unordered_map<char, std::bitset<32>>> search(pattern);
 
-BOOST_AUTO_TEST_CASE(first_of_two_overlapping_patterns_is_found)
-{
-    std::string pattern("шалаш");
-    std::string text("   шалашалаш   ");
-    thrust::algorithm::bitap<char, std::bitset<32>, std::unordered_map<char, std::bitset<32>>> search(pattern);
+        auto match_position = search(text.begin(), text.end());
 
-    auto match_position = search(text.begin(), text.end());
+        typedef typename std::iterator_traits<std::string::iterator>::difference_type difference_type;
+        difference_type non_space_offset = static_cast<difference_type>(text.find_first_not_of(" "));
+        BOOST_CHECK(match_position == text.begin() + non_space_offset);
+    }
 
-    typedef typename std::iterator_traits<std::string::iterator>::difference_type difference_type;
-    difference_type non_space_offset = static_cast<difference_type>(text.find_first_not_of(" "));
-    BOOST_CHECK(match_position == text.begin() + non_space_offset);
-}
+    BOOST_AUTO_TEST_CASE(forward_iterator_is_enough_for_searching)
+    {
+        std::forward_list<int> pattern{1, 2, 3};
+        std::forward_list<int> text{0, 1, 2, 3, 4};
+        thrust::algorithm::bitap<int, std::bitset<32>, std::unordered_map<int, std::bitset<32>>> search(pattern);
 
-BOOST_AUTO_TEST_CASE(forward_iterator_is_enough_for_searching)
-{
-    std::forward_list<int> pattern{1, 2, 3};
-    std::forward_list<int> text{0, 1, 2, 3, 4};
-    thrust::algorithm::bitap<int, std::bitset<32>, std::unordered_map<int, std::bitset<32>>> search(pattern);
+        auto match_position = search(text.begin(), text.end());
 
-    auto match_position = search(text.begin(), text.end());
+        BOOST_CHECK(match_position == std::find(text.begin(), text.end(), 1));
+    }
 
-    BOOST_CHECK(match_position == std::find(text.begin(), text.end(), 1));
-}
+    BOOST_AUTO_TEST_CASE(copied_search_object_performs_the_same_search)
+    {
+        std::string pattern("два");
+        std::string text("дваждыдвачетыре");
+        thrust::algorithm::bitap<char, std::uint32_t, std::unordered_map<char, std::uint32_t>> search(pattern);
+        auto copied_search = search;
 
-BOOST_AUTO_TEST_CASE(copied_search_object_performs_the_same_search)
-{
-    std::string pattern("два");
-    std::string text("дваждыдвачетыре");
-    thrust::algorithm::bitap<char, std::uint32_t, std::unordered_map<char, std::uint32_t>> search(pattern);
-    auto copied_search = search;
-
-    auto initial_search_result = search(text.begin(), text.end());
-    auto copied_search_result = copied_search(text.begin(), text.end());
-    BOOST_CHECK(initial_search_result == copied_search_result);
-}
-
+        auto initial_search_result = search(text.begin(), text.end());
+        auto copied_search_result = copied_search(text.begin(), text.end());
+        BOOST_CHECK(initial_search_result == copied_search_result);
+    }
 BOOST_AUTO_TEST_SUITE_END()
--- test/algorithm/searching/element_position_bitmask_table.cpp
+++ test/algorithm/searching/element_position_bitmask_table.cpp
@@ -12,49 +12,47 @@ typedef thrust::algorithm::detail::element_position_bitmask_table<std::map<char,
 typedef thrust::algorithm::detail::element_position_bitmask_table<std::map<int, std::bitset<64>>> integral_masks;
 
 BOOST_AUTO_TEST_SUITE(ElementPositionBitmaskTable)
-
-BOOST_AUTO_TEST_CASE(mask_created_from_empty_pattern_is_empty)
-{
-    char_masks pattern_masks(std::string(""));
-    BOOST_CHECK_EQUAL(pattern_masks.length(), 0ul);
-}
-
-BOOST_AUTO_TEST_CASE(one_element_pattern)
-{
-    char_masks pattern_masks(std::string("a"));
-    BOOST_CHECK_EQUAL(pattern_masks['a'], BOOST_BINARY(1));
-}
-
-BOOST_AUTO_TEST_CASE(pattern_with_repeating_elements)
-{
-    integral_masks pattern_masks{1, 2, 1, 3};
-    BOOST_CHECK_EQUAL(pattern_masks[1], BOOST_BINARY(0101));
-    BOOST_CHECK_EQUAL(pattern_masks[2], BOOST_BINARY(0010));
-    BOOST_CHECK_EQUAL(pattern_masks[3], BOOST_BINARY(1000));
-}
-
-BOOST_AUTO_TEST_CASE(bitmask_for_nonexistent_element_is_zero)
-{
-    char_masks pattern_masks(std::string("qwertyqwerty"));
-
-    BOOST_CHECK_EQUAL(pattern_masks['a'], BOOST_BINARY(0));
-    BOOST_CHECK_EQUAL(pattern_masks['b'], BOOST_BINARY(0));
-}
-
-BOOST_AUTO_TEST_CASE(disjunction_of_masks_for_every_pattern_element_has_all_bits_set_to_1)
-{
-    std::string pattern("abac");
-    char_masks pattern_masks(pattern);
-
-    std::set<char> unique_pattern_elements(pattern.begin(), pattern.end());
-
-    char_masks::bitmask_type disjunction = 0;
-    for (char element: unique_pattern_elements)
+    BOOST_AUTO_TEST_CASE(mask_created_from_empty_pattern_is_empty)
     {
-        disjunction |= pattern_masks[element];
+        char_masks pattern_masks(std::string(""));
+        BOOST_CHECK_EQUAL(pattern_masks.length(), 0ul);
     }
 
-    BOOST_CHECK_EQUAL(disjunction, (0x01 << pattern.length()) - 1);
-}
+    BOOST_AUTO_TEST_CASE(one_element_pattern)
+    {
+        char_masks pattern_masks(std::string("a"));
+        BOOST_CHECK_EQUAL(pattern_masks['a'], BOOST_BINARY(1));
+    }
 
+    BOOST_AUTO_TEST_CASE(pattern_with_repeating_elements)
+    {
+        integral_masks pattern_masks{1, 2, 1, 3};
+        BOOST_CHECK_EQUAL(pattern_masks[1], BOOST_BINARY(0101));
+        BOOST_CHECK_EQUAL(pattern_masks[2], BOOST_BINARY(0010));
+        BOOST_CHECK_EQUAL(pattern_masks[3], BOOST_BINARY(1000));
+    }
+
+    BOOST_AUTO_TEST_CASE(bitmask_for_nonexistent_element_is_zero)
+    {
+        char_masks pattern_masks(std::string("qwertyqwerty"));
+
+        BOOST_CHECK_EQUAL(pattern_masks['a'], BOOST_BINARY(0));
+        BOOST_CHECK_EQUAL(pattern_masks['b'], BOOST_BINARY(0));
+    }
+
+    BOOST_AUTO_TEST_CASE(disjunction_of_masks_for_every_pattern_element_has_all_bits_set_to_1)
+    {
+        std::string pattern("abac");
+        char_masks pattern_masks(pattern);
+
+        std::set<char> unique_pattern_elements(pattern.begin(), pattern.end());
+
+        char_masks::bitmask_type disjunction = 0;
+        for (char element: unique_pattern_elements)
+        {
+            disjunction |= pattern_masks[element];
+        }
+
+        BOOST_CHECK_EQUAL(disjunction, (0x01 << pattern.length()) - 1);
+    }
 BOOST_AUTO_TEST_SUITE_END()
