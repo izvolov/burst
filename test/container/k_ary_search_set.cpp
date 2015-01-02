4b9ff2b32140f53730847f60a81203395b52585c
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-01-03 00:21:12 +0300

    refs #18 Множество на k-разрядном плоском дереве поиска.

---
+++ test/container/k_ary_search_set.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+
+#include <thrust/container/k_ary_search_set.hpp>
+
+BOOST_AUTO_TEST_SUITE(k_ary_search)
+    BOOST_AUTO_TEST_CASE(k_ary_search_set_is_empty_when_initialized_from_empty_range)
+    {
+        std::vector<int> nothing;
+
+        thrust::k_ary_search_set<int> set(nothing.begin(), nothing.end());
+
+        BOOST_CHECK(set.empty());
+    }
+
+    BOOST_AUTO_TEST_CASE(size_of_k_ary_search_set_initialized_from_empty_range_is_zero)
+    {
+        std::vector<int> nothing;
+
+        thrust::k_ary_search_set<int> set(nothing.begin(), nothing.end());
+
+        BOOST_CHECK_EQUAL(set.size(), 0);
+    }
+
+    BOOST_AUTO_TEST_CASE(k_ary_search_set_size_is_equal_to_size_of_range_it_is_initialized_from)
+    {
+        std::vector<int> something{1, 2, 3, 4};
+
+        thrust::k_ary_search_set<int> set(something.begin(), something.end());
+
+        BOOST_CHECK_EQUAL(set.size(), something.size());
+    }
+
+    BOOST_AUTO_TEST_CASE(k_ary_search_set_is_initialized_properly_when_it_is_perfect_ternary_tree)
+    {
+        std::vector<int> numbers{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+        thrust::k_ary_search_set<int> set(numbers.begin(), numbers.end(), 3);
+
+        std::vector<int> expected{7, 10, 2, 5, 8, 9, 11, 12, 0, 1, 3, 4, 6};
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            set.begin(), set.end(),
+            expected.begin(), expected.end()
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(k_ary_search_set_is_initialized_properly_when_it_is_complete_ternary_tree)
+    {
+        std::vector<int> numbers{0, 1, 2, 3, 4, 5, 6, 7};
+
+        thrust::k_ary_search_set<int> set(numbers.begin(), numbers.end(), 3);
+
+        std::vector<int> expected{2, 5, 0, 1, 3, 4, 6, 7};
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            set.begin(), set.end(),
+            expected.begin(), expected.end()
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(k_ary_search_set_is_initialized_properly_when_arity_is_greater_than_its_size)
+    {
+        std::vector<int> numbers{0, 1, 2, 3, 4, 5, 6, 7};
+
+        thrust::k_ary_search_set<int> set(numbers.begin(), numbers.end(), 129);
+
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            set.begin(), set.end(),
+            numbers.begin(), numbers.end()
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(nothing_is_found_when_searching_empty_range)
+    {
+        std::vector<int> nothing;
+
+        thrust::k_ary_search_set<int> set(nothing.begin(), nothing.end());
+
+        BOOST_CHECK(set.find(26) == set.end());
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_absent_value_results_end_iterator)
+    {
+        std::vector<int> numbers{3, 4, 6, 1, 7, 8, 2};
+        std::sort(numbers.begin(), numbers.end());
+
+        thrust::k_ary_search_set<int> set(numbers.begin(), numbers.end());
+
+        BOOST_CHECK(set.find(26) == set.end());
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_existing_value_results_non_end_iterator)
+    {
+        std::vector<int> numbers{3, 4, 6, 1, 7, 8, 2};
+        std::sort(numbers.begin(), numbers.end());
+
+        thrust::k_ary_search_set<int> set(numbers.begin(), numbers.end());
+
+        BOOST_CHECK(set.find(8) != set.end());
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_existing_value_results_iterator_pointing_to_that_value)
+    {
+        std::vector<int> numbers{3, 4, 6, 1, 7, 8, 2};
+        std::sort(numbers.begin(), numbers.end(), std::greater<int>());
+
+        thrust::k_ary_search_set<int, std::greater<int>> set(numbers.begin(), numbers.end());
+
+        BOOST_CHECK_EQUAL(*set.find(2), 2);
+    }
+BOOST_AUTO_TEST_SUITE_END()
---
+++ thrust/container/k_ary_search_set.hpp
@@ -0,0 +1,237 @@
+#ifndef THRUST_CONTAINER_K_ARY_SEARCH_SET_HPP
+#define THRUST_CONTAINER_K_ARY_SEARCH_SET_HPP
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iterator>
+#include <stack>
+#include <utility>
+#include <vector>
+
+#include <boost/assert.hpp>
+#include <boost/range/iterator_range.hpp>
+
+namespace thrust
+{
+    struct k_ary_search_set_branch
+    {
+        std::size_t index;
+        std::size_t size;
+        std::size_t height;
+        std::size_t preceding_elements;
+    };
+
+    template<typename Value, typename Compare = std::less<Value>>
+    class k_ary_search_set
+    {
+    public:
+        using value_type = Value;
+        using value_compare = Compare;
+
+    private:
+        using value_container_type = std::vector<value_type>;
+
+    public:
+        using iterator = typename value_container_type::iterator;
+        using const_iterator = typename value_container_type::const_iterator;
+        using size_type = typename value_container_type::size_type;
+        using difference_type = typename value_container_type::difference_type;
+
+    public:
+        template <typename RandomAccessIterator>
+        k_ary_search_set
+                (
+                    RandomAccessIterator first,
+                    RandomAccessIterator last,
+                    std::size_t arity = 129,
+                    const value_compare & compare = value_compare()
+                ):
+            m_values(static_cast<size_type>(std::distance(first, last))),
+            m_arity(arity),
+            m_compare(compare)
+        {
+            BOOST_ASSERT(std::is_sorted(first, last, compare));
+
+            if (first != last)
+            {
+                initialize(boost::make_iterator_range(first, last));
+            }
+        }
+
+    public:
+        iterator find (const value_type & value)
+        {
+            std::size_t node_index = 0;
+
+            while (node_index < m_values.size())
+            {
+                iterator first = begin() + static_cast<difference_type>(node_index);
+                iterator last = begin() + std::min(static_cast<difference_type>(node_index + m_arity - 1), std::distance(begin(), end()));
+
+                iterator search_result = std::lower_bound(first, last, value, m_compare);
+                if (search_result != last && not m_compare(*search_result, value))
+                {
+                    return search_result;
+                }
+                else
+                {
+                    node_index = perfect_tree_child_index
+                    (
+                        m_arity,
+                        node_index,
+                        static_cast<std::size_t>(std::distance(first, search_result))
+                    );
+                }
+            }
+
+            return end();
+        }
+
+        const_iterator find (const value_type & value) const
+        {
+            return find(value);
+        }
+
+        size_type size () const
+        {
+            return m_values.size();
+        }
+
+        bool empty () const
+        {
+            return m_values.empty();
+        }
+
+        iterator begin ()
+        {
+            return m_values.begin();
+        }
+
+        iterator end ()
+        {
+            return m_values.end();
+        }
+
+        const_iterator begin () const
+        {
+            return m_values.begin();
+        }
+
+        const_iterator end () const
+        {
+            return m_values.end();
+        }
+
+        const_iterator cbegin () const
+        {
+            return m_values.cbegin();
+        }
+
+        const_iterator cend () const
+        {
+            return m_values.cend();
+        }
+
+    private:
+        template <typename RandomAccessRange>
+        void initialize (const RandomAccessRange & range)
+        {
+            std::stack<k_ary_search_set_branch> branches;
+
+            branches.push({0, size(), perfect_tree_height(m_arity, size()), 0});
+            while (not branches.empty())
+            {
+                const auto branch = branches.top();
+                branches.pop();
+
+                // Количество меньших элементов ветки для каждого элемента текущего узла.
+                std::vector<std::size_t> counters;
+                fill_counters(branch, counters);
+
+                fill_node(branch, counters, range);
+
+                if (counters[0] > 0)
+                {
+                    branches.push
+                    ({
+                        perfect_tree_child_index(m_arity, branch.index, 0),
+                        counters[0],
+                        branch.height - 1,
+                        branch.preceding_elements
+                    });
+                }
+
+                for (std::size_t i = 1; i < counters.size() && (counters[i] - counters[i - 1] - 1) > 0; ++i)
+                {
+                    branches.push
+                    ({
+                        perfect_tree_child_index(m_arity, branch.index, i),
+                        counters[i] - counters[i - 1] - 1,
+                        branch.height - 1,
+                        branch.preceding_elements + counters[i - 1] + 1
+                    });
+                }
+            }
+        }
+
+        void fill_counters (const k_ary_search_set_branch & branch, std::vector<std::size_t> & counters)
+        {
+            const std::size_t max_subtree_height = branch.height - 1;
+            const std::size_t min_subtree_elements = perfect_tree_size(m_arity, max_subtree_height - 1);
+            const std::size_t max_subtree_elements = perfect_tree_size(m_arity, max_subtree_height);
+            const std::size_t elements_in_last_row = branch.size - perfect_tree_size(m_arity, branch.height - 1);
+
+            counters.resize(std::min(m_arity, branch.size + 1));
+            for (std::size_t i = 0; i < counters.size(); ++i)
+            {
+                counters[i] = i + std::min
+                (
+                    (i + 1) * min_subtree_elements + elements_in_last_row,
+                    (i + 1) * max_subtree_elements
+                );
+            }
+            BOOST_ASSERT(counters.back() == branch.size);
+        }
+
+        template <typename RandomAccessRange>
+        void fill_node (const k_ary_search_set_branch & branch, const std::vector<std::size_t> & counters, const RandomAccessRange & range)
+        {
+            for (std::size_t element_index = 0; element_index < counters.size() - 1; ++element_index)
+            {
+                using difference_type = typename RandomAccessRange::difference_type;
+                const auto index_in_initial_range = static_cast<difference_type>(branch.preceding_elements + counters[element_index]);
+                m_values[branch.index + element_index] = range[index_in_initial_range];
+            }
+            BOOST_ASSERT(std::is_sorted
+            (
+                m_values.begin() + static_cast<typename value_container_type::difference_type>(branch.index),
+                m_values.begin() + static_cast<typename value_container_type::difference_type>(branch.index + counters.size() - 1),
+                m_compare
+            ));
+        }
+
+    private:
+        static std::size_t perfect_tree_size (std::size_t arity, std::size_t height)
+        {
+            return static_cast<std::size_t>(std::pow(arity, height)) - 1;
+        }
+
+        static std::size_t perfect_tree_height (std::size_t arity, std::size_t size)
+        {
+            return static_cast<std::size_t>(std::floor(std::log(size) / std::log(arity))) + 1;
+        }
+
+        static std::size_t perfect_tree_child_index (std::size_t arity, std::size_t parent_index, std::size_t child_number)
+        {
+            return parent_index * arity + (child_number + 1) * (arity - 1);
+        }
+
+    private:
+        value_container_type m_values;
+        const std::size_t m_arity;
+        value_compare m_compare;
+    };
+}
+
+#endif // THRUST_CONTAINER_K_ARY_SEARCH_SET_HPP
