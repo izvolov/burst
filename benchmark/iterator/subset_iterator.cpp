eb9a7cb6ba6c2cd50ec521ea8988b38a1637bedf
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-01-21 20:52:59 +0300

    refs #13 Испытания скорости.

---
+++ benchmark/iterator/subset_iterator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+#include <burst/iterator/subset_iterator.hpp>
+
+template <typename Container>
+void read (std::istream & stream, Container & values)
+{
+    for (typename Container::value_type n; stream >> n; /* пусто */)
+    {
+        values.push_back(n);
+    }
+}
+
+template <typename Container>
+void test (const Container & numbers)
+{
+    auto begin = burst::make_subset_iterator(numbers);
+    auto end = burst::make_subset_iterator(numbers, burst::iterator::end_tag);
+
+    clock_t total_time = 0;
+    std::size_t iteration_count = 0;
+    while (begin != end)
+    {
+        clock_t increment_time = clock();
+        ++begin;
+        increment_time = clock() - increment_time;
+        total_time += increment_time;
+        ++iteration_count;
+    }
+
+    std::cout << "Общее время: " << static_cast<double>(total_time) / CLOCKS_PER_SEC << std::endl;
+    std::cout << "Среднее время: " << static_cast<double>(total_time) / iteration_count / CLOCKS_PER_SEC << std::endl;
+}
+
+int main ()
+{
+    std::vector<std::int64_t> numbers;
+    read(std::cin, numbers);
+
+    test(numbers);
+}
