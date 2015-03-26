e7ddd4556eab7a65e82927de25aa2b95fde0252b
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-03-26 22:13:32 +0300

    refs #19 Скачущий поиск верхней грани.

---
+++ burst/algorithm/galloping_upper_bound.hpp
@@ -0,0 +1,87 @@
+#ifndef BURST_ALGORITHM_GALLOPING_UPPER_BOUND_HPP
+#define BURST_ALGORITHM_GALLOPING_UPPER_BOUND_HPP
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+#include <boost/assert.hpp>
+
+namespace burst
+{
+    //!     "Скачущий" поиск верхней грани.
+    /*!
+            Эффективен для поиска значения в упорядоченном диапазоне, если известно, что искомый
+        элемент находится близко к началу диапазона.
+            Входной диапазон должен быть либо упорядочен относительно заданного отношения порядка,
+        либо, по крайней мере, разделён относительно искомого значения, то есть во входном
+        диапазоне все элементы, меньшие искомого относительно заданного отношения порядка, должны
+        стоять слева от него, а все, которые не меньше, — справа.
+
+        \tparam RandomAccessIterator
+            Тип итератора принимаемого на вход диапазона. Должен быть итератором произвольного
+            доступа.
+        \tparam Value
+            Тип искомого значения. Должен быть сравнимым с элементами диапазона [first, last) в
+            качестве левого операнда.
+        \tparam Compare
+            Бинарная операция, задающая отношение строгого порядка на элементах входного диапазона.
+            Если пользователем явно не указана операция, то, по-умолчанию, берётся отношение
+            "меньше", задаваемое функциональным объектом "std::less<T>".
+        \return RandomAccessIterator
+            Возвращает итератор на первый элемент в диапазоне, который больше искомого относительно
+            заданного порядка, либо, если в диапазоне такого элемента нет, итератор на конец
+            диапазона.
+
+            Алгоритм работы.
+
+        1. На первый элемент диапазона устанавливается бегунок.
+        2. Устанавливается первоначальный размер скачка (например, 1).
+        3. Бегунок совершает скачок.
+        4. Элемент, на котором стоит бегунок, сравнивается с искомым.
+           а. Если элемент не больше искомого, то размер скачка увеличивается вдвое и бегунок снова
+              совершает скачок.
+           б. Если бегунок выскочил за пределы диапазона или элемент больше искомого, то искомый
+              элемент доискивается двоичным поиском между предыдущим и текущим положением бегунка.
+
+            Асимптотика.
+
+        Время: O(logN), N = |[first, last)| — размер диапазона.
+        Память: O(1).
+     */
+    template <typename RandomAccessIterator, typename Value, typename Compare>
+    RandomAccessIterator galloping_upper_bound (RandomAccessIterator first, RandomAccessIterator last, const Value & value, Compare compare)
+    {
+        BOOST_ASSERT(std::is_sorted(first, last, compare));
+
+        auto distance = std::distance(first, last);
+        using difference_type = typename std::iterator_traits<RandomAccessIterator>::difference_type;
+        auto position = static_cast<difference_type>(0);
+        auto step = static_cast<difference_type>(1);
+
+        while (position + step < distance)
+        {
+            auto current = std::next(first, step);
+            if (not compare(value, *current))
+            {
+                first = std::next(current);
+                position += step + 1;
+                step *= 2;
+            }
+            else
+            {
+                return std::upper_bound(first, current, value, compare);
+            }
+        }
+
+        return std::upper_bound(first, last, value, compare);
+    }
+
+    template <typename RandomAccessIterator, typename Value>
+    RandomAccessIterator galloping_upper_bound (RandomAccessIterator first, RandomAccessIterator last, const Value & value)
+    {
+        return galloping_upper_bound(first, last, value, std::less<Value>());
+    }
+} // namespace burst
+
+#endif // BURST_ALGORITHM_GALLOPING_UPPER_BOUND_HPP
---
+++ test/algorithm/galloping_upper_bound.cpp
@@ -0,0 +1,77 @@
+#include <functional>
+#include <list>
+#include <string>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+
+#include <burst/algorithm/galloping_upper_bound.hpp>
+
+BOOST_AUTO_TEST_SUITE(galloping_upper_bound)
+    BOOST_AUTO_TEST_CASE(searching_an_empty_range_results_range_end)
+    {
+        std::vector<int> empty;
+
+        auto search_result = burst::galloping_upper_bound(empty.begin(), empty.end(), 1);
+
+        BOOST_CHECK(search_result == empty.end());
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_a_value_greater_than_all_values_in_a_given_range_results_range_end)
+    {
+        std::vector<std::size_t> range{1, 2, 3, 4, 5};
+
+        auto search_result = burst::galloping_upper_bound(range.begin(), range.end(), 100500ul);
+
+        BOOST_CHECK(search_result == range.end());
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_a_value_less_than_all_values_in_a_given_range_results_range_begin)
+    {
+        std::list<std::int8_t> range{10, 20, 30, 40};
+
+        auto search_result = burst::galloping_upper_bound(range.begin(), range.end(), 0);
+
+        BOOST_CHECK(search_result == range.begin());
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_absent_value_results_iterator_to_first_value_greater_than_desired)
+    {
+        auto range = {50, 40, 30, 20, 10};
+
+        auto search_result = burst::galloping_upper_bound(range.begin(), range.end(), 33, std::greater<int>());
+
+        BOOST_CHECK(search_result == range.begin() + 2);
+        BOOST_CHECK_EQUAL(*search_result, 30);
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_existing_value_results_iterator_to_first_value_greater_than_desired)
+    {
+        std::vector<std::string> range{"abc", "bc", "c"};
+
+        auto search_result = burst::galloping_upper_bound(range.begin(), range.end(), std::string("bc"));
+
+        BOOST_CHECK(search_result == range.begin() + 2);
+        BOOST_CHECK_EQUAL(*search_result, "c");
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_existing_value_results_iterator_past_the_end_of_range_of_equal_values)
+    {
+        std::vector<int> range{1, 1, 1, 2, 2, 2, 3, 3, 3};
+
+        auto search_result = burst::galloping_upper_bound(range.begin(), range.end(), 2);
+
+        BOOST_CHECK(search_result == range.begin() + 6);
+        BOOST_CHECK_EQUAL(*search_result, 3);
+    }
+
+    BOOST_AUTO_TEST_CASE(searching_absent_value_results_iterator_to_beginning_of_range_of_equal_values)
+    {
+        std::vector<int> range{30, 30, 30, 20, 20, 20, 10, 10, 10};
+
+        auto search_result = burst::galloping_upper_bound(range.begin(), range.end(), 25, std::greater<int>());
+
+        BOOST_CHECK(search_result == range.begin() + 3);
+        BOOST_CHECK_EQUAL(*search_result, 20);
+    }
+BOOST_AUTO_TEST_SUITE_END()
