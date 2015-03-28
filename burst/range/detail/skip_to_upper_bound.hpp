300ac4c13db91a7e89207841f74748fb4baa0c59
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-03-28 15:15:13 +0300

    Алгоритм прокрутки диапазона до верхней грани.

---
+++ burst/range/detail/skip_to_upper_bound.hpp
@@ -0,0 +1,70 @@
+#ifndef BURST_RANGE_DETAIL_SKIP_TO_UPPER_BOUND_HPP
+#define BURST_RANGE_DETAIL_SKIP_TO_UPPER_BOUND_HPP
+
+#include <type_traits>
+#include <iterator>
+
+#include <boost/range/algorithm/upper_bound.hpp>
+#include <boost/range/iterator_range.hpp>
+
+namespace burst
+{
+    namespace detail
+    {
+        //!     Продвижение диапазона произвольного доступа.
+        /*!
+                Использует двоичный поиск для нахождения в диапазоне места, до которого нужно
+            продвинуться, и сразу продвигается до этого места.
+         */
+        template <typename RandomAccessRange, typename Value, typename Compare>
+        typename std::enable_if
+        <
+            std::is_same
+            <
+                std::random_access_iterator_tag,
+                typename std::iterator_traits<typename boost::range_iterator<RandomAccessRange>::type>::iterator_category
+            >
+            ::value,
+            void
+        >
+        ::type
+        skip_to_upper_bound (RandomAccessRange & range, const Value & goal, Compare compare)
+        {
+            range.advance_begin
+            (
+                std::distance
+                (
+                    range.begin(),
+                    boost::upper_bound(range, goal, compare)
+                )
+            );
+        }
+
+        //!     Продвижение любого диапазона, кроме произвольного доступа.
+        /*!
+                Поэлементно двигает начало диапазона до тех пор, пока либо диапазон не закончится,
+            либо его начало не встанет на то место, до которого нужно продвинуться.
+         */
+        template <typename InputRange, typename Value, typename Compare>
+        typename std::enable_if
+        <
+            not std::is_same
+            <
+                std::random_access_iterator_tag,
+                typename std::iterator_traits<typename boost::range_iterator<InputRange>::type>::iterator_category
+            >
+            ::value,
+            void
+        >
+        ::type
+        skip_to_upper_bound (InputRange & range, const Value & goal, Compare compare)
+        {
+            while (not range.empty() && not compare(goal, range.front()))
+            {
+                range.advance_begin(1);
+            }
+        }
+    }
+}
+
+#endif // BURST_RANGE_DETAIL_SKIP_TO_UPPER_BOUND_HPP
---
+++ test/range/skip_to_upper_bound.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <functional>
+
+#include <boost/range/iterator_range.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <burst/range/skip_to_upper_bound.hpp>
+
+BOOST_AUTO_TEST_SUITE(skip_to_upper_bound)
+    BOOST_AUTO_TEST_CASE(skipping_by_value_in_empty_range_results_empty_range)
+    {
+        std::vector<int> empty_container;
+        auto empty_range = boost::make_iterator_range(empty_container);
+
+        burst::skip_to_upper_bound(empty_range, 5);
+
+        BOOST_CHECK(empty_range.empty());
+    }
+
+    BOOST_AUTO_TEST_CASE(skipping_by_existing_value_makes_range_begin_after_last_occurence_of_the_value)
+    {
+        std::vector<int> values{1, 5, 5, 7};
+        auto sorted_range = boost::make_iterator_range(values);
+
+        burst::skip_to_upper_bound(sorted_range, 5);
+
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::begin(sorted_range), boost::end(sorted_range),
+            boost::begin(values) + 3, boost::end(values)
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(skipping_by_value_greater_than_all_values_in_range_makes_range_empty)
+    {
+        auto values = {1, 2, 3};
+        auto sorted_range = boost::make_iterator_range(values);
+
+        burst::skip_to_upper_bound(sorted_range, 100500);
+
+        BOOST_CHECK(sorted_range.empty());
+    }
+
+    BOOST_AUTO_TEST_CASE(skipping_by_value_less_than_all_elements_in_range_does_not_modify_range)
+    {
+        auto values = {3, 2, 1};
+        auto sorted_range = boost::make_iterator_range(values);
+        auto sorted_range_copy = sorted_range;
+
+        burst::skip_to_upper_bound(sorted_range, 100500, std::greater<int>());
+
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::begin(sorted_range), boost::end(sorted_range),
+            boost::begin(sorted_range_copy), boost::end(sorted_range_copy)
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(skipping_by_value_absent_in_sorted_range_makes_range_begin_at_first_value_greater_than_desired)
+    {
+        std::vector<double> values{3.14, 2.71, 1.6};
+        auto sorted_range = boost::make_iterator_range(values);
+
+        burst::skip_to_upper_bound(sorted_range, 2.5, std::greater<double>());
+
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            boost::begin(sorted_range), boost::end(sorted_range),
+            boost::begin(values) + 2, boost::end(values)
+        );
+    }
+BOOST_AUTO_TEST_SUITE_END()
