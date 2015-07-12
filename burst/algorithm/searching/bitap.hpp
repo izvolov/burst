4cf77f6d183ccfee2ea3daf99e99d64ab5492469
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-07-12 14:23:18 +0300

    refs #24 Отображение в виде массива используется только для однобайтовых.

--- burst/algorithm/searching/bitap.hpp
+++ burst/algorithm/searching/bitap.hpp
@@ -37,7 +37,7 @@ namespace burst
             typename Map = typename std::conditional
             <
                 std::is_integral<Value>::value && sizeof(Value) == 1,
-                std::array<Bitmask, (1ul << (sizeof(Value) * CHAR_BIT))>,
+                std::array<Bitmask, (1ul << CHAR_BIT)>,
                 std::map<Value, Bitmask>
             >
             ::type
