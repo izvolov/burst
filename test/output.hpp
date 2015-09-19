76531d88ffffd2938a18455a083e71dd05ef7266
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-09-19 23:16:47 +0300

    Тестовый вывод вектора.

--- test/output.hpp
+++ test/output.hpp
@@ -1,8 +1,11 @@
 #ifndef BURST_TEST_OUTPUT_HPP
 #define BURST_TEST_OUTPUT_HPP
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <utility>
+#include <vector>
 
 namespace std
 {
@@ -10,6 +13,24 @@ namespace std
     std::ostream & operator << (std::ostream & stream, const std::pair<First, Second> & pair)
     {
         return stream << "{" << pair.first << ", " << pair.second << "}";
+    }
+
+    template <typename T>
+    std::ostream & operator << (std::ostream & stream, const std::vector<T> & vector)
+    {
+        stream << "[";
+
+        if (not vector.empty())
+        {
+            stream << vector.front();
+            std::for_each(std::next(vector.begin()), vector.end(),
+                [& stream] (const auto & value)
+                {
+                    stream << ", " << value;
+                });
+        }
+
+        return stream << "]";
     }
 }
 
