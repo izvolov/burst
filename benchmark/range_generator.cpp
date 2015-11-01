d25c7ea2c303cbd08963677522290766e399ec2f
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-11-01 13:46:52 +0300

    refs #17 Возможность генерировать разные последовательности

--- benchmark/range_generator.cpp
+++ benchmark/range_generator.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <chrono>
 #include <cstdint>
 #include <exception>
 #include <functional>
@@ -9,9 +10,12 @@
 #include <boost/program_options.hpp>
 #include <boost/range/algorithm/for_each.hpp>
 
-void generate (std::size_t range_count, std::size_t range_length, std::int64_t min, std::int64_t max, bool sort, bool descending)
+void generate (std::size_t range_count, std::size_t range_length, std::int64_t min, std::int64_t max, bool seed, bool sort, bool descending)
 {
-    std::default_random_engine engine;
+    auto seed_value = seed
+        ? static_cast<std::default_random_engine::result_type>(std::chrono::system_clock::now().time_since_epoch().count())
+        : 0;
+    std::default_random_engine engine(seed_value);
     std::uniform_int_distribution<std::int64_t> uniform(min, max);
 
     std::vector<std::int64_t> range(range_length);
@@ -46,6 +50,7 @@ int main (int argc, const char * argv[])
         ("length", bpo::value<std::size_t>(), "Длина одного набора")
         ("min", bpo::value<std::int64_t>()->default_value(std::numeric_limits<std::int64_t>::min()), "Минимальное значение элемента набора")
         ("max", bpo::value<std::int64_t>()->default_value(std::numeric_limits<std::int64_t>::max()), "Максимальное значение элемента набора")
+        ("seed", bpo::value<bool>()->implicit_value(true)->default_value(false), "Выбрать произвольную точку начала последовательности псевдослучайных чисел")
         ("sort", bpo::value<bool>()->implicit_value(true)->default_value(false), "Породить упорядоченный набор")
         ("descending", bpo::value<bool>()->implicit_value(true)->default_value(false), "Упорядочить набор по убыванию");
 
@@ -66,10 +71,11 @@ int main (int argc, const char * argv[])
             std::int64_t min = vm["min"].as<std::int64_t>();
             std::int64_t max = vm["max"].as<std::int64_t>();
 
+            bool seed = vm["seed"].as<bool>();
             bool sort = vm["sort"].as<bool>();
             bool descending = vm["descending"].as<bool>();
 
-            generate(range_count, range_length, min, max, sort, descending);
+            generate(range_count, range_length, min, max, seed, sort, descending);
         }
     }
     catch (std::exception &)
