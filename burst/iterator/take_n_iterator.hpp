6f254ff53a7b5c4e33aec619638032493e4ad147
Дмитрий Изволов <dmitriy@izvolov.ru>
2015-12-24 18:57:30 +0300

    refs #35 Откусывающий итератор

---
+++ burst/iterator/take_n_iterator.hpp
@@ -0,0 +1,138 @@
+#ifndef BURST_ITERATOR_TAKE_N_ITERATOR_HPP
+#define BURST_ITERATOR_TAKE_N_ITERATOR_HPP
+
+#include <iterator>
+#include <type_traits>
+
+#include <boost/iterator/iterator_categories.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+
+#include <burst/iterator/end_tag.hpp>
+
+namespace burst
+{
+    //!     Итератор, "откусывающий" от диапазона.
+    /*!
+            Присасывается к любому итератору I и откусывает первые n элементов, то есть назначает
+        ему новый конец при помощи счётчика n. Иначе говоря, новый конец входного итератора I будет
+        ровно I + n.
+
+            Ответственность за допустимое продвижение итератора в данном случае лежит на
+        пользователе. Это значит, что, если настоящий конец итератора I находится ближе, чем I + n,
+        то такая ситуация влечёт за собой неопределённое поведение.
+
+            Категория этого итератора — минимум из однонаправленной и категории входного итератора.
+     */
+    template <typename Iterator>
+    class take_n_iterator:
+        public boost::iterator_facade
+        <
+            take_n_iterator<Iterator>,
+            typename std::iterator_traits<Iterator>::value_type,
+            typename std::common_type
+            <
+                boost::forward_traversal_tag,
+                typename boost::iterators::pure_iterator_traversal<Iterator>::type
+            >
+            ::type,
+            typename std::iterator_traits<Iterator>::reference,
+            typename std::iterator_traits<Iterator>::difference_type
+        >
+    {
+    private:
+        using base_type = boost::iterator_facade
+        <
+            take_n_iterator,
+            typename std::iterator_traits<Iterator>::value_type,
+            typename std::common_type
+            <
+                boost::forward_traversal_tag,
+                typename boost::iterators::pure_iterator_traversal<Iterator>::type
+            >
+            ::type,
+            typename std::iterator_traits<Iterator>::reference,
+            typename std::iterator_traits<Iterator>::difference_type
+        >;
+
+    public:
+        take_n_iterator (Iterator iterator, typename base_type::difference_type items_to_take):
+            m_iterator(iterator),
+            m_remaining(items_to_take)
+        {
+        }
+
+        take_n_iterator (iterator::end_tag_t):
+            m_iterator{},
+            m_remaining(0)
+        {
+        }
+
+    private:
+        friend class boost::iterator_core_access;
+
+        void increment ()
+        {
+            ++m_iterator;
+            --m_remaining;
+        }
+
+        typename base_type::difference_type distance_to (const take_n_iterator & that) const
+        {
+            return this->m_remaining - that.m_remaining;
+        }
+
+        typename base_type::reference dereference () const
+        {
+            return *m_iterator;
+        }
+
+        bool equal (const take_n_iterator & that) const
+        {
+            return this->m_remaining == that.m_remaining;
+        }
+
+    private:
+        Iterator m_iterator;
+        typename base_type::difference_type m_remaining;
+    };
+
+    //!     Функция для создания начала откусанного диапазона.
+    /*!
+            Принимает итератор и число.
+            Возвращает новый итератор, конец которого равен std::next(iterator, n).
+
+            Асимптотика: O(1) по времени и памяти.
+     */
+    template <typename Iterator>
+    take_n_iterator<Iterator>
+        make_take_n_iterator
+        (
+            Iterator iterator,
+            typename std::iterator_traits<Iterator>::difference_type n
+        )
+    {
+        return take_n_iterator<Iterator>(iterator, n);
+    }
+
+    //!     Функция для создания конца откусанного диапазона.
+    /*!
+            Принимает на вход экземпляр откусывающего итератора, формально задающего начало
+        диапазона. На самом деле этот аргумент не используется, а нужен только для вывода типа
+        итератора.
+            Возвращает итератор на конец откусанного диапазона.
+
+            Асимптотика: O(1) по времени и памяти.
+     */
+    template <typename Iterator>
+    take_n_iterator<Iterator>
+        make_take_n_iterator
+        (
+            const take_n_iterator<Iterator> &,
+            iterator::end_tag_t
+        )
+    {
+        return take_n_iterator<Iterator>(iterator::end_tag);
+    }
+} // namespace burst
+
+#endif // BURST_ITERATOR_TAKE_N_ITERATOR_HPP
---
+++ test/iterator/take_n_iterator.cpp
@@ -0,0 +1,84 @@
+#include <iterator>
+#include <sstream>
+
+#include <boost/range/concepts.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <burst/container/make_forward_list.hpp>
+#include <burst/container/make_list.hpp>
+#include <burst/container/make_vector.hpp>
+#include <burst/iterator/take_n_iterator.hpp>
+
+BOOST_AUTO_TEST_SUITE(take_n_iterator)
+    BOOST_AUTO_TEST_CASE(iterator_category_falls_back_to_forward_iterator)
+    {
+        // Входной итератор — произвольного доступа
+        {
+            const auto v = burst::make_vector({1, 2, 3});
+
+            auto initial = v.begin();
+            BOOST_CONCEPT_ASSERT((boost::RandomAccessIterator<decltype(initial)>));
+
+            auto take = burst::make_take_n_iterator(initial, 2);
+            BOOST_CONCEPT_ASSERT((boost::ForwardIterator<decltype(take)>));
+        }
+
+        // Входной итератор двунаправленный
+        {
+            const auto l = burst::make_list({4, 5, 6, 7});
+
+            auto initial = l.begin();
+            BOOST_CONCEPT_ASSERT((boost::BidirectionalIterator<decltype(initial)>));
+
+            auto take = burst::make_take_n_iterator(initial, 3);
+            BOOST_CONCEPT_ASSERT((boost::ForwardIterator<decltype(take)>));
+        }
+
+        // Входной итератор однонаправленный
+        {
+            const auto fl = burst::make_forward_list({'q', 'w', 'e'});
+
+            auto initial = fl.begin();
+            BOOST_CONCEPT_ASSERT((boost::ForwardIterator<decltype(initial)>));
+
+            auto take = burst::make_take_n_iterator(initial, 1);
+            BOOST_CONCEPT_ASSERT((boost::ForwardIterator<decltype(take)>));
+        }
+
+        // Входной итератор однопроходный
+        {
+            std::stringstream stream("1 2 3 4");
+
+            auto initial = std::istream_iterator<int>(stream);
+            BOOST_CONCEPT_ASSERT((boost::InputIterator<decltype(initial)>));
+
+            auto take = burst::make_take_n_iterator(initial, 3);
+            BOOST_CONCEPT_ASSERT((boost::InputIterator<decltype(take)>));
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(end_is_created_using_special_tag)
+    {
+        const auto l = burst::make_list({1, 2, 3, 4});
+
+        auto first = burst::make_take_n_iterator(l.begin(), 3);
+        auto last = burst::make_take_n_iterator(first, burst::iterator::end_tag);
+
+        BOOST_CHECK(std::next(first, 3) == last);
+    }
+
+    BOOST_AUTO_TEST_CASE(takes_exactly_specified_number_of_elements)
+    {
+        const auto fl = burst::make_forward_list({1, 2, 3, 4});
+
+        const auto items_to_take = 2;
+        auto first = burst::make_take_n_iterator(fl.begin(), items_to_take);
+        auto last = burst::make_take_n_iterator(first, burst::iterator::end_tag);
+
+        BOOST_CHECK_EQUAL_COLLECTIONS
+        (
+            fl.begin(), std::next(fl.begin(), items_to_take),
+            first, last
+        );
+    }
+BOOST_AUTO_TEST_SUITE_END()
