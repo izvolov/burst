41cdcc856547d3d421682a9a8d34c76e32e65735
Дмитрий Изволов <dmitriy@izvolov.ru>
2016-01-23 22:32:01 +0300

    Улучшение терминологии

--- burst/math/intpow.hpp
+++ burst/math/intpow.hpp
@@ -14,20 +14,20 @@ namespace burst
                 Раскладывает показатель на степени двойки и последовательно перемножает основание в
             тех степенях двойки, которые встретились в этом разложении.
          */
-        template <typename Base, typename Exponent>
-        constexpr auto intpow_impl (Base x, Exponent n)
+        template <typename Integer1, typename Integer2>
+        constexpr auto intpow_impl (Integer1 x, Integer2 exponent)
         {
-            auto result = Base{1};
+            auto result = Integer1{1};
 
-            while (n > 0)
+            while (exponent > 0)
             {
-                if ((n & 0x01) != 0)
+                if ((exponent & 0x01) != 0)
                 {
                     result *= x;
                 }
 
                 x *= x;
-                n >>= 1;
+                exponent >>= 1;
             }
 
             return result;
@@ -39,14 +39,14 @@ namespace burst
             Так называемый "быстрый алгоритм".
             Время работы: O(logN).
      */
-    template <typename Base, typename Exponent>
-    constexpr auto intpow (Base x, Exponent n)
+    template <typename Integer1, typename Integer2>
+    constexpr auto intpow (Integer1 base, Integer2 exponent)
     {
-        static_assert(std::is_integral<Base>::value, "Основание должно быть целым.");
-        static_assert(std::is_integral<Exponent>::value, "Показатель степени должен быть целым.");
+        static_assert(std::is_integral<Integer1>::value, "Основание должно быть целым.");
+        static_assert(std::is_integral<Integer2>::value, "Показатель степени должен быть целым.");
 
-        return n >= 0
-            ? detail::intpow_impl(x, n)
+        return exponent >= 0
+            ? detail::intpow_impl(base, exponent)
             : throw std::logic_error("Отрицательная степень целого числа не может быть представлена целым числом");
     }
 } // namespace burst
