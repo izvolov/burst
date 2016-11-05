d770ac02725e15c6c01be655033380b88aa3862e
Дмитрий Изволов <dmitriy@izvolov.ru>
2016-11-05 22:00:24 +0300

    refs #50 Новая концепция функции burst::each
    Теперь each не является полноценным функциональным объектом. Это лишь сигнал для композиции функций о том, что эту самую композицию нужно провести по-другому.
    each не возвращает кортеж элементов, а прокидывает отображённые аргументы в функцию, с которой была произведена композиция.

--- burst/tuple/each.hpp
+++ burst/tuple/each.hpp
@@ -3,32 +3,53 @@
 
 #include <burst/functional/compose.hpp>
 
-#include <tuple>
+#include <type_traits>
 #include <utility>
 
 namespace burst
 {
     //!     Применение функции к множеству объектов
     /*!
-            Принимает на вход произвольный набор объектов, к каждому из них применяет заданную
-        функцию, и результат применений сохраняет в кортеже в соответственном порядке.
+            Своего рода "сигнал" для функции композиции, с помощью которого можно указать, что
+        композиция должна происходить несколько иначе, чем обычно (см. `each_compose_fn`).
      */
     template <typename UnaryFunction>
     struct each_fn
     {
+        UnaryFunction f;
+    };
+
+    //!     Поэлементная композиция
+    /*!
+            Принимает произвольный набор аргументов. К каждому аргументу применяется запомненная
+        ранее функция `R`, а затем этот набор отображённых аргументов передаётся в функцию 'L',
+        также запомненную ранее.
+            Иными словами, входные аргументы "прокидываются" в функцию 'L', при этом каждый из них
+        попутно преобразовывается при помощи функции 'R'.
+     */
+    template <typename L, typename R>
+    struct each_compose_fn
+    {
         template <typename ... Ts>
         constexpr decltype(auto) operator () (Ts && ... ts) const &
         {
-            return std::make_tuple(f(std::forward<Ts>(ts))...);
+            return l(r(std::forward<Ts>(ts))...);
         }
 
         template <typename ... Ts>
         constexpr decltype(auto) operator () (Ts && ... ts) &
         {
-            return std::make_tuple(f(std::forward<Ts>(ts))...);
+            return l(r(std::forward<Ts>(ts))...);
         }
 
-        UnaryFunction f;
+        template <typename ... Ts>
+        constexpr decltype(auto) operator () (Ts && ... ts) &&
+        {
+            return std::move(l)(r(std::forward<Ts>(ts))...);
+        }
+
+        L l;
+        R r;
     };
 
     template <typename UnaryFunction>
@@ -36,6 +57,29 @@ namespace burst
         -> each_fn<std::decay_t<UnaryFunction>>
     {
         return {std::forward<UnaryFunction>(f)};
+    }
+
+    //!     Композиция двух поэлементных преобразований
+    /*!
+            Возвращает новое поэлементное преобразование, созданное из композиции внутренних
+        функций входных поэлементых преобразований.
+     */
+    template <typename UnaryFunction1, typename UnaryFunction2>
+    constexpr auto compose (each_fn<UnaryFunction1> l, each_fn<UnaryFunction2> r)
+        -> each_fn<decltype(compose(l.f, r.f))>
+    {
+        return {compose(std::move(l.f), std::move(r.f))};
+    }
+
+    //!     Композиция поэлементного преобразования с обычной функцией
+    /*!
+            См. `each_compose_fn`.
+     */
+    template <typename L, typename UnaryFunction>
+    constexpr auto compose (L && l, each_fn<UnaryFunction> r)
+        -> each_compose_fn<std::decay_t<L>, UnaryFunction>
+    {
+        return {std::forward<L>(l), std::move(r.f)};
     }
 
     template <typename UnaryFunction, typename R>
--- test/tuple/each.cpp
+++ test/tuple/each.cpp
@@ -1,5 +1,7 @@
+#include <burst/algorithm/sum.hpp>
 #include <burst/integer/intlog2.hpp>
 #include <burst/tuple/each.hpp>
+#include <burst/tuple/make_tuple.hpp>
 #include <output.hpp>
 
 #include <boost/test/unit_test.hpp>
@@ -8,11 +10,19 @@
 #include <tuple>
 
 BOOST_AUTO_TEST_SUITE(each)
-    BOOST_AUTO_TEST_CASE(saves_application_of_specified_function_to_all_input_arguments_in_tuple)
+    BOOST_AUTO_TEST_CASE(forwards_input_arguments_mapped_by_specified_function)
     {
-        auto e = burst::each([] (auto x) {return x + x;});
+        auto e = burst::each([] (auto x) {return x + x;}) | burst::make_tuple;
         auto t = e(4, std::string("qwe"));
         BOOST_CHECK_EQUAL(t, std::make_tuple(8, "qweqwe"));
+    }
+
+    BOOST_AUTO_TEST_CASE(may_be_composed_with_another_each)
+    {
+        const auto square = [] (auto x) {return x * x;};
+        auto e = burst::each(square) | burst::each(square) | burst::sum;
+        auto r = e(1, 2, 3);
+        BOOST_CHECK_EQUAL(r, 1 + 16 + 81);
     }
 
     struct doubler
@@ -35,7 +45,7 @@ BOOST_AUTO_TEST_SUITE(each)
     BOOST_AUTO_TEST_CASE(calls_inner_function_on_every_tuple_element)
     {
         auto calls = std::size_t{0};
-        auto e = burst::each(doubler{calls});
+        auto e = burst::each(doubler{calls}) | burst::make_tuple;
 
         e(4, 'a', 3.14);
 
@@ -69,14 +79,14 @@ BOOST_AUTO_TEST_SUITE(each)
 
     int dummy::instances_count = 0;
 
-    BOOST_AUTO_TEST_CASE(passed_function_is_stored_inside)
+    BOOST_AUTO_TEST_CASE(every_passed_function_is_stored_inside)
     {
         const auto old_instances_count = dummy::instances_count;
 
-        auto e = burst::each(dummy{});
+        auto e = burst::each(dummy{}) | burst::each(dummy{});
         static_cast<void>(e);
 
-        BOOST_CHECK_EQUAL(dummy::instances_count, old_instances_count + 1);
+        BOOST_CHECK_EQUAL(dummy::instances_count, old_instances_count + 2);
     }
 
     BOOST_AUTO_TEST_CASE(function_is_not_stored_if_passed_by_ref)
@@ -116,7 +126,7 @@ BOOST_AUTO_TEST_SUITE(each)
     BOOST_AUTO_TEST_CASE(stored_function_invokes_as_const_lvalue_if_each_is_const_lvalue)
     {
         auto calls = std::size_t{0};
-        const auto each = burst::each(const_lvalue_caller{calls});
+        const auto each = burst::each(const_lvalue_caller{calls}) | burst::make_tuple;
 
         each(1, "qwe");
 
@@ -149,17 +159,47 @@ BOOST_AUTO_TEST_SUITE(each)
     BOOST_AUTO_TEST_CASE(stored_function_invokes_as_lvalue_if_each_is_lvalue)
     {
         auto calls = std::size_t{0};
-        auto each = burst::each(lvalue_caller{calls});
+        auto each = burst::each(lvalue_caller{calls}) | burst::make_tuple;
 
         each(1, "qwe");
 
         BOOST_CHECK_EQUAL(calls, 2);
     }
 
-    BOOST_AUTO_TEST_CASE(stored_function_invokes_as_const_lvalue_if_each_is_rvalue)
+    struct rvalue_caller
+    {
+        rvalue_caller (std::size_t & calls):
+            calls(calls)
+        {
+        }
+
+        template <typename Ts>
+        void operator () (Ts ...) const & = delete;
+
+        template <typename Ts>
+        void operator () (Ts ...) & = delete;
+
+        template <typename Ts>
+        void operator () (Ts ...) &&
+        {
+            ++calls;
+        }
+
+        std::size_t & calls;
+    };
+
+    BOOST_AUTO_TEST_CASE(composed_function_invokes_as_rvalue_when_each_is_rvalue)
     {
         auto calls = std::size_t{0};
-        burst::each(const_lvalue_caller{calls})(1, "qwe");
+        (burst::each([] (auto x) {return x + x;}) | rvalue_caller{calls})(1, 2, 3);
+
+        BOOST_CHECK_EQUAL(calls, 1);
+    }
+
+    BOOST_AUTO_TEST_CASE(stored_function_invokes_as_lvalue_if_each_is_rvalue)
+    {
+        auto calls = std::size_t{0};
+        (burst::each(lvalue_caller{calls}) | burst::make_tuple)(1, "qwe");
 
         BOOST_CHECK_EQUAL(calls, 2);
     }
@@ -168,7 +208,7 @@ BOOST_AUTO_TEST_SUITE(each)
     {
         auto calls = std::size_t{0};
         auto c = lvalue_caller{calls};
-        const auto each = burst::each(std::ref(c));
+        const auto each = burst::each(std::ref(c)) | burst::make_tuple;
 
         each(1, "qwe");
 
@@ -179,7 +219,7 @@ BOOST_AUTO_TEST_SUITE(each)
     {
         auto calls = std::size_t{0};
         const auto c = const_lvalue_caller{calls};
-        const auto each = burst::each(std::ref(c));
+        const auto each = burst::each(std::ref(c)) | burst::make_tuple;
 
         each(1, "qwe");
 
@@ -188,7 +228,7 @@ BOOST_AUTO_TEST_SUITE(each)
 
     BOOST_AUTO_TEST_CASE(is_a_constexpr_function)
     {
-        constexpr auto e = burst::each(&burst::intlog2<int>);
+        constexpr auto e = burst::each(&burst::intlog2<int>) | burst::make_tuple;
         constexpr auto t = e(1, 256, 1024);
         BOOST_CHECK_EQUAL(t, std::make_tuple(0, 8, 10));
     }
