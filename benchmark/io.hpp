d2b4ff4fa6d0af62efa9ae587dec74d5233eadd6
Дмитрий Изволов <dmitriy@izvolov.ru>
2016-12-14 17:31:41 +0300

    refs #17 Бинарный формат общения в инфраструктуре замеров

--- benchmark/io.hpp
+++ benchmark/io.hpp
@@ -1,25 +1,42 @@
 #ifndef BURST_BENCHMARK_IO_HPP
 #define BURST_BENCHMARK_IO_HPP
 
-#include <boost/range/algorithm/for_each.hpp>
+#include <burst/functional/trivial_read.hpp>
+#include <burst/functional/trivial_write.hpp>
+#include <burst/iterator/binary_ostream_iterator.hpp>
 
+#include <boost/range/algorithm/transform.hpp>
+
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
-#include <sstream>
-#include <string>
 
 template <typename Container>
 std::ostream & write (std::ostream & stream, const Container & values)
 {
-    boost::for_each(values, [& stream] (std::int64_t value) { stream << value << ' '; });
+    burst::trivial_write(stream, static_cast<std::size_t>(values.size()));
+    boost::transform(values, burst::make_binary_ostream_iterator(stream),
+        [] (std::int64_t v) {return v;});
+    // Элементы контейнера должны уметь неявно (то есть без потерь) преобразовываться к
+    // std::int64_t, т.к. это "официальный" формат обмена числами между приложениями.
+
     return stream;
 }
 
 template <typename Container>
 std::istream & read (std::istream & stream, Container & values)
 {
-    for (std::int64_t n; stream >> n; /* пусто */)
+    auto size = std::size_t{};
+    if (burst::trivial_read(stream, size))
     {
-        values.push_back(static_cast<typename Container::value_type>(n));
+        values.resize(size);
+
+        for (auto & value: values)
+        {
+            auto n = std::int64_t{};
+            burst::trivial_read(stream, n);
+            value = static_cast<typename Container::value_type>(n);
+        }
     }
 
     return stream;
@@ -28,12 +45,10 @@ std::istream & read (std::istream & stream, Container & values)
 template <typename Container>
 std::istream & read_many (std::istream & stream, Container & containers)
 {
-    for (std::string line; std::getline(stream, line); /* пусто */)
+    auto container = typename Container::value_type{};
+    while (read(stream, container))
     {
-        containers.resize(containers.size() + 1);
-
-        std::stringstream line_stream(line);
-        read(line_stream, containers.back());
+        containers.push_back(container);
     }
 
     return stream;
