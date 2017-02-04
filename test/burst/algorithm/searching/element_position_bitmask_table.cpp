0ce2c6745b6985cbc56e4cb4b3936f5b8ed39e30
Дмитрий Изволов <dmitriy@izvolov.ru>
2017-02-05 01:07:38 +0300

    refs #60 Приведение тестов в порядок

--- test/burst/algorithm/searching/element_position_bitmask_table.cpp
+++ test/burst/algorithm/searching/element_position_bitmask_table.cpp
@@ -1,7 +1,7 @@
 #include <burst/algorithm/searching/detail/element_position_bitmask_table.hpp>
 
+#include <boost/range/numeric.hpp>
 #include <boost/test/unit_test.hpp>
-#include <boost/utility/binary.hpp>
 
 #include <array>
 #include <bitset>
@@ -14,46 +14,60 @@ using char_masks = burst::algorithm::detail::element_position_bitmask_table<std:
 using integral_masks = burst::algorithm::detail::element_position_bitmask_table<std::map<int, std::bitset<64>>>;
 
 BOOST_AUTO_TEST_SUITE(element_position_bitmask_table)
-    BOOST_AUTO_TEST_CASE(mask_created_from_empty_pattern_is_empty)
+    BOOST_AUTO_TEST_CASE(length_of_bitmask_table_created_from_empty_pattern_is_zero)
     {
-        char_masks pattern_masks(std::string(""));
+        const auto pattern_masks = char_masks(std::string(""));
         BOOST_CHECK_EQUAL(pattern_masks.length(), 0ul);
     }
 
-    BOOST_AUTO_TEST_CASE(one_element_pattern)
+    BOOST_AUTO_TEST_CASE(length_of_bitmask_table_created_from_non_empty_pattern_is_equal_to_length_of_the_pattern)
     {
-        char_masks pattern_masks(std::string("a"));
-        BOOST_CHECK_EQUAL(pattern_masks['a'], BOOST_BINARY(1));
+        const auto pattern_masks = char_masks(std::string("abcabc"));
+        BOOST_CHECK_EQUAL(pattern_masks.length(), 6);
     }
 
-    BOOST_AUTO_TEST_CASE(pattern_with_repeating_elements)
+    BOOST_AUTO_TEST_CASE(bits_in_all_positions_of_an_element_are_set_up)
     {
-        integral_masks pattern_masks{1, 2, 1, 3};
-        BOOST_CHECK_EQUAL(pattern_masks[1], BOOST_BINARY(0101));
-        BOOST_CHECK_EQUAL(pattern_masks[2], BOOST_BINARY(0010));
-        BOOST_CHECK_EQUAL(pattern_masks[3], BOOST_BINARY(1000));
+        const auto pattern_masks = integral_masks{1, 2, 1, 3};
+        BOOST_CHECK_EQUAL(pattern_masks[1], 0b0101);
+        BOOST_CHECK_EQUAL(pattern_masks[2], 0b0010);
+        BOOST_CHECK_EQUAL(pattern_masks[3], 0b1000);
+    }
+
+    BOOST_AUTO_TEST_CASE(all_bits_higher_than_length_of_the_pattern_are_not_set)
+    {
+        const auto pattern = std::string("abc");
+        const auto pattern_masks = char_masks(pattern);
+        const auto unique_pattern_elements = std::set<char>(pattern.begin(), pattern.end());
+        const auto all_ones = ~char_masks::bitmask_type{0b0};
+
+        for (auto element: unique_pattern_elements)
+        {
+            const auto mask = pattern_masks[element];
+            BOOST_CHECK_EQUAL(mask & all_ones, mask);
+        }
     }
 
     BOOST_AUTO_TEST_CASE(bitmask_for_nonexistent_element_is_zero)
     {
-        char_masks pattern_masks(std::string("qwertyqwerty"));
+        const auto pattern_masks = char_masks(std::string("qwertyqwerty"));
 
-        BOOST_CHECK_EQUAL(pattern_masks['a'], BOOST_BINARY(0));
-        BOOST_CHECK_EQUAL(pattern_masks['b'], BOOST_BINARY(0));
+        BOOST_CHECK_EQUAL(pattern_masks['a'], 0b0);
+        BOOST_CHECK_EQUAL(pattern_masks['b'], 0b0);
     }
 
-    BOOST_AUTO_TEST_CASE(disjunction_of_masks_for_every_pattern_element_has_all_bits_set_to_1)
+    BOOST_AUTO_TEST_CASE(disjunction_of_masks_for_every_pattern_element_has_all_pattern_bits_set_to_1)
     {
-        std::string pattern("abac");
-        char_masks pattern_masks(pattern);
+        const auto pattern = std::string("abac");
+        const auto pattern_masks = char_masks(pattern);
 
-        std::set<char> unique_pattern_elements(pattern.begin(), pattern.end());
-
-        char_masks::bitmask_type disjunction = 0;
-        for (char element: unique_pattern_elements)
-        {
-            disjunction |= pattern_masks[element];
-        }
+        const auto unique_pattern_elements = std::set<char>(pattern.begin(), pattern.end());
+        const auto disjunction =
+            boost::accumulate(unique_pattern_elements, char_masks::bitmask_type{0b0},
+                [& pattern_masks] (auto mask, auto element)
+                {
+                    return mask | pattern_masks[element];
+                });
 
         BOOST_CHECK_EQUAL(disjunction, (0b1 << pattern.length()) - 1);
     }
