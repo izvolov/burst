e21bca9252a7a9d59ed989f1a9f5bdc25b9985ef
Дмитрий Изволов <dmitriy@izvolov.ru>
2017-03-25 16:11:33 +0300

    refs #64 Все части алгоритма в одном пространстве имён

--- burst/algorithm/next_subset.hpp
+++ burst/algorithm/next_subset.hpp
@@ -7,88 +7,85 @@
 
 namespace burst
 {
-    namespace detail
+    //!     Заполнение подмножества.
+    /*!
+            Принимает подмножество, заданное полуинтервалом [subset_begin, subset_end),
+        последовательность, заданную полуинтервалом [sequence_begin, sequence_end), а также
+        отношение порядка на элементах последовательности (сама последовательность должна быть
+        упорядочена относительно этой операции).
+            Пытается заполнить подмножество итераторами на разные элементы последовательности.
+            Возвращает итератор, указывающий за последний заполненный элемент подмножества.
+        Если всё подмножество удалось заполнить, то возвращаемый результат равен subset_end.
+        Если не всё подмножество удалось заполнить, то результирующий итератор указывает
+        куда-то в полуинтервал [subset_begin, subset_end).
+     */
+    template
+    <
+        typename ForwardIterator1,
+        typename ForwardIterator2,
+        typename Compare
+    >
+    ForwardIterator1
+        fill_subset
+        (
+            ForwardIterator1 subset_begin, ForwardIterator1 subset_end,
+            ForwardIterator2 sequence_begin, ForwardIterator2 sequence_end,
+            Compare compare
+        )
     {
-        //!     Заполнение подмножества.
-        /*!
-                Принимает подмножество, заданное полуинтервалом [subset_begin, subset_end),
-            последовательность, заданную полуинтервалом [sequence_begin, sequence_end), а также
-            отношение порядка на элементах последовательности (сама последовательность должна быть
-            упорядочена относительно этой операции).
-                Пытается заполнить подмножество итераторами на разные элементы последовательности.
-                Возвращает итератор, указывающий за последний заполненный элемент подмножества.
-            Если всё подмножество удалось заполнить, то возвращаемый результат равен subset_end.
-            Если не всё подмножество удалось заполнить, то результирующий итератор указывает
-            куда-то в полуинтервал [subset_begin, subset_end).
-         */
-        template
-        <
-            typename ForwardIterator1,
-            typename ForwardIterator2,
-            typename Compare
-        >
-        ForwardIterator1
-            fill_subset
-            (
-                ForwardIterator1 subset_begin, ForwardIterator1 subset_end,
-                ForwardIterator2 sequence_begin, ForwardIterator2 sequence_end,
-                Compare compare
-            )
+        while (sequence_begin != sequence_end && subset_begin != subset_end)
         {
-            while (sequence_begin != sequence_end && subset_begin != subset_end)
-            {
-                *subset_begin++ = sequence_begin;
-                sequence_begin =
-                    std::upper_bound(std::next(sequence_begin), sequence_end, *sequence_begin, compare);
-            }
-
-            return subset_begin;
+            *subset_begin++ = sequence_begin;
+            sequence_begin =
+                std::upper_bound(std::next(sequence_begin), sequence_end, *sequence_begin, compare);
         }
 
-        //!     Переход к следующему подмножеству фиксированного размера.
-        /*!
-                Принимает подмножество, заданное полуинтервалом [subset_begin, subset_end),
-            последовательность, заданную полуинтервалом [sequence_begin, sequence_end), а также
-            отношение порядка на элементах последовательности (сама последовательность должна быть
-            упорядочена относительно этой операции).
-                Пытается найти новое подмножество размера |[subset_begin, subset_end)|.
-                Возвращает итератор, указывающий за последний действительный элемент подмножества.
-            Если новое подмножество удалось найти, то возвращаемый результат равен subset_end. Если
-            не удалось, то результирующий итератор указывает куда-то в полуинтервал
-            [subset_begin, subset_end).
-         */
-        template <typename BidirectionalIterator, typename ForwardIterator, typename Compare>
-        BidirectionalIterator
-            next_fixed_size_subset
-            (
-                BidirectionalIterator subset_begin, BidirectionalIterator subset_end,
-                ForwardIterator /* sequence_begin */, ForwardIterator sequence_end,
-                Compare compare
-            )
-        {
-            auto subset_rbegin = std::make_reverse_iterator(subset_end);
-            auto subset_rend = std::make_reverse_iterator(subset_begin);
+        return subset_begin;
+    }
 
-            auto moving = subset_rbegin;
-            while (moving != subset_rend)
+    //!     Переход к следующему подмножеству фиксированного размера.
+    /*!
+            Принимает подмножество, заданное полуинтервалом [subset_begin, subset_end),
+        последовательность, заданную полуинтервалом [sequence_begin, sequence_end), а также
+        отношение порядка на элементах последовательности (сама последовательность должна быть
+        упорядочена относительно этой операции).
+            Пытается найти новое подмножество размера |[subset_begin, subset_end)|.
+            Возвращает итератор, указывающий за последний действительный элемент подмножества.
+        Если новое подмножество удалось найти, то возвращаемый результат равен subset_end. Если
+        не удалось, то результирующий итератор указывает куда-то в полуинтервал
+        [subset_begin, subset_end).
+     */
+    template <typename BidirectionalIterator, typename ForwardIterator, typename Compare>
+    BidirectionalIterator
+        next_fixed_size_subset
+        (
+            BidirectionalIterator subset_begin, BidirectionalIterator subset_end,
+            ForwardIterator /* sequence_begin */, ForwardIterator sequence_end,
+            Compare compare
+        )
+    {
+        auto subset_rbegin = std::make_reverse_iterator(subset_end);
+        auto subset_rend = std::make_reverse_iterator(subset_begin);
+
+        auto moving = subset_rbegin;
+        while (moving != subset_rend)
+        {
+            *moving = std::upper_bound(std::next(*moving), sequence_end, **moving, compare);
+            if (*moving != sequence_end)
             {
-                *moving = std::upper_bound(std::next(*moving), sequence_end, **moving, compare);
-                if (*moving != sequence_end)
+                auto last_filled =
+                    fill_subset(std::prev(moving.base()), subset_end, *moving, sequence_end, compare);
+                if (last_filled == subset_end)
                 {
-                    auto last_filled =
-                        fill_subset(std::prev(moving.base()), subset_end, *moving, sequence_end, compare);
-                    if (last_filled == subset_end)
-                    {
-                        return subset_end;
-                    }
+                    return subset_end;
                 }
-
-                ++moving;
             }
 
-            return moving.base();
+            ++moving;
         }
-    } // namespace detail
+
+        return moving.base();
+    }
 
     //!     Переход к следующему подмножеству.
     /*!
@@ -117,10 +114,10 @@ namespace burst
         )
     {
         if (subset_begin == subset_end
-            || detail::next_fixed_size_subset(subset_begin, subset_end, sequence_begin, sequence_end, compare) != subset_end)
+            || next_fixed_size_subset(subset_begin, subset_end, sequence_begin, sequence_end, compare) != subset_end)
         {
             ++subset_end;
-            auto last_filled = detail::fill_subset(subset_begin, subset_end, sequence_begin, sequence_end, compare);
+            auto last_filled = fill_subset(subset_begin, subset_end, sequence_begin, sequence_end, compare);
             if (last_filled != subset_end)
             {
                 subset_end = subset_begin;
