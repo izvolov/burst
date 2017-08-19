f61d2ba24fa098d1fa792fcd705f64df2988da67
Дмитрий Изволов <dmitriy@izvolov.ru>
2017-08-19 14:04:27 +0300

    refs #72 Устранён лишний аргумент метафункции

--- burst/type_traits/aligned_volume.hpp
+++ burst/type_traits/aligned_volume.hpp
@@ -3,31 +3,29 @@
 
 #include <burst/integer/intceil.hpp>
 
-#include <algorithm>
 #include <cstddef>
 
 namespace burst
 {
     namespace detail
     {
-        template <std::size_t Total, std::size_t MaxAlignment, typename ... Tail>
+        template <std::size_t Total, typename ... Types>
         struct aligned_volume_impl;
 
-        template <std::size_t Total, std::size_t MaxAlignment, typename Head, typename ... Tail>
-        struct aligned_volume_impl <Total, MaxAlignment, Head, Tail...>
+        template <std::size_t Total, typename Head, typename ... Tail>
+        struct aligned_volume_impl <Total, Head, Tail...>
         {
             static constexpr auto value =
                 aligned_volume_impl
                 <
                     intceil(Total, alignof(Head)) + sizeof(Head),
-                    std::max(MaxAlignment, alignof(Head)),
                     Tail...
                 >
                 ::value;
         };
 
-        template <std::size_t Total, std::size_t MaxAlignment>
-        struct aligned_volume_impl <Total, MaxAlignment>
+        template <std::size_t Total>
+        struct aligned_volume_impl <Total>
         {
             static constexpr auto value = Total;
         };
@@ -42,7 +40,7 @@ namespace burst
     template <typename ... Args>
     struct aligned_volume
     {
-        static constexpr auto value = detail::aligned_volume_impl<0, 0, Args...>::value;
+        static constexpr auto value = detail::aligned_volume_impl<0, Args...>::value;
     };
 } // namespace burst
 
