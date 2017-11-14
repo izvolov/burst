6d0a41d2ad68be6ff1197d1411830bf5f6a22832
Дмитрий Изволов <dmitriy@izvolov.ru>
2017-11-14 22:30:09 +0300

    refs #78 Деление с округлением до целого

---
+++ burst/integer/detail/divceil.hpp
@@ -0,0 +1,91 @@
+#ifndef BURST_INTEGER_DETAIL_DIVCEIL_HPP
+#define BURST_INTEGER_DETAIL_DIVCEIL_HPP
+
+#include <limits>
+#include <stdexcept>
+#include <type_traits>
+
+namespace burst
+{
+    namespace detail
+    {
+        template <typename I>
+        constexpr I divceil_positive (I n, I d)
+        {
+            return (n - I{1}) / d + I{1};
+        }
+
+        template <typename I>
+        constexpr I divceil_negative (I n, I d)
+        {
+            return n / d;
+        }
+
+        template <typename I, typename J>
+        constexpr auto divceil_impl (I n, J d)
+            -> std::enable_if_t<std::is_unsigned<I>::value, I>
+        {
+            if (n > 0)
+            {
+                using K = std::common_type_t<I, J>;
+                return static_cast<I>(divceil_positive(static_cast<K>(n), static_cast<K>(d)));
+            }
+            else
+            {
+                return I{0};
+            }
+        }
+
+        template <typename I, typename J>
+        constexpr auto divceil_impl (I n, J d)
+            ->
+                std::enable_if_t
+                <
+                    std::is_signed<I>::value &&
+                        (std::is_signed<J>::value ||
+                            (std::is_unsigned<J>::value
+                                && std::is_signed<std::common_type_t<I, J>>::value)),
+                    I
+                >
+        {
+            if (n > 0)
+            {
+                using K = std::common_type_t<I, J>;
+                return static_cast<I>(divceil_positive(static_cast<K>(n), static_cast<K>(d)));
+            }
+            else
+            {
+                using K = std::common_type_t<I, J>;
+                return static_cast<I>(divceil_negative(static_cast<K>(n), static_cast<K>(d)));
+            }
+        }
+
+        template <typename I, typename J>
+        constexpr auto divceil_impl (I n, J d)
+            ->
+                std::enable_if_t
+                <
+                    std::is_signed<I>::value &&
+                        std::is_unsigned<J>::value
+                            && std::is_same<std::common_type_t<I, J>, J>::value,
+                    I
+                >
+        {
+            if (n > 0)
+            {
+                using K = std::common_type_t<I, J>;
+                return static_cast<I>(divceil_positive(static_cast<K>(n), static_cast<K>(d)));
+            }
+            else if (d <= J{std::numeric_limits<I>::max()})
+            {
+                return divceil_negative(n, static_cast<I>(d));
+            }
+            else
+            {
+                return I{0};
+            }
+        }
+    }
+}
+
+#endif // BURST_INTEGER_DETAIL_DIVCEIL_HPP
---
+++ burst/integer/divceil.hpp
@@ -0,0 +1,31 @@
+#ifndef BURST_INTEGER_DIVCEIL_HPP
+#define BURST_INTEGER_DIVCEIL_HPP
+
+#include <burst/concept/integer.hpp>
+#include <burst/integer/detail/divceil.hpp>
+
+#include <stdexcept>
+
+namespace burst
+{
+    //!     Целая часть от деления, "округлённая" вверх
+    /*!
+            Принимает два целых числа: `n` и `divisor`.
+            Возвращает наименьшее число `m` такое, что `m * divisor >= n`.
+            Тип возвращаемого результата всегда равен типу первого аргумента.
+     */
+    template <typename I, typename J>
+    constexpr auto divceil (Integer<I> n, Integer<J> divisor)
+    {
+        if (divisor > 0)
+        {
+            return detail::divceil_impl(n, divisor);
+        }
+        else
+        {
+            throw std::domain_error("Целая часть от деления на неположительное число не определена.");
+        }
+    }
+} // namespace burst
+
+#endif // BURST_INTEGER_DIVCEIL_HPP
