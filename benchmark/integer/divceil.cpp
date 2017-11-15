5d48b1256e104b15daf8c3a56e7fafdec043cb6b
Дмитрий Изволов <dmitriy@izvolov.ru>
2017-11-15 12:47:34 +0300

    refs #78 Возможность выбора типов делимого и делителя при замерах

--- benchmark/integer/divceil.cpp
+++ benchmark/integer/divceil.cpp
@@ -2,15 +2,18 @@
 #include <io.hpp>
 
 #include <boost/program_options.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_io.hpp>
 
 #include <chrono>
 #include <cstdint>
 #include <iostream>
 #include <numeric>
+#include <unordered_map>
 #include <vector>
 
-template <typename Container>
-void test_divceil (const Container & values, std::size_t attempt_count)
+template <typename Container, typename Integer>
+void test_divceil (const Container & values, Integer divisor, std::size_t attempt_count)
 {
     using namespace std::chrono;
     using value_type = typename Container::value_type;
@@ -20,7 +23,6 @@ void test_divceil (const Container & values, std::size_t attempt_count)
     auto total = value_type{0};
     for (std::size_t iteration = 0; iteration < attempt_count; ++iteration)
     {
-        const auto divisor = 100;
         total +=
             std::accumulate(values.begin(), values.end(), value_type{0},
                 [divisor] (auto a, auto b)
@@ -36,6 +38,70 @@ void test_divceil (const Container & values, std::size_t attempt_count)
     std::cout << "Время: " << time << std::endl;
 }
 
+template <typename Integer1, typename Integer2>
+void test_all (std::int64_t divisor, std::size_t attempt_count)
+{
+    std::vector<Integer1> values;
+    read(std::cin, values);
+
+    test_divceil(values, static_cast<Integer2>(divisor), attempt_count);
+}
+
+using test_call_type = void (*) (std::int64_t, std::size_t);
+template <typename Integer>
+test_call_type dispatch_divisor (const std::string & divisor_type)
+{
+    static const auto test_calls =
+        std::unordered_map<std::string, test_call_type>
+        {
+            {"uint8", &test_all<Integer, std::uint8_t>},
+            {"uint16", &test_all<Integer, std::uint16_t>},
+            {"uint32", &test_all<Integer, std::uint32_t>},
+            {"uint64", &test_all<Integer, std::uint64_t>},
+            {"int8", &test_all<Integer, std::int8_t>},
+            {"int16", &test_all<Integer, std::int16_t>},
+            {"int32", &test_all<Integer, std::int32_t>},
+            {"int64", &test_all<Integer, std::int64_t>}
+        };
+
+    auto call = test_calls.find(divisor_type);
+    if (call != test_calls.end())
+    {
+        return call->second;
+    }
+    else
+    {
+        throw boost::program_options::error(u8"Неверная разрядность делителя: " + divisor_type);
+    }
+}
+
+test_call_type get_call (const std::string & integer_type, const std::string & divisor_type)
+{
+    using divisor_dispatcher = test_call_type (*) (const std::string &);
+    static const auto dispatchers =
+        std::unordered_map<std::string, divisor_dispatcher>
+        {
+            {"uint8", &dispatch_divisor<std::uint8_t>},
+            {"uint16", &dispatch_divisor<std::uint16_t>},
+            {"uint32", &dispatch_divisor<std::uint32_t>},
+            {"uint64", &dispatch_divisor<std::uint64_t>},
+            {"int8", &dispatch_divisor<std::int8_t>},
+            {"int16", &dispatch_divisor<std::int16_t>},
+            {"int32", &dispatch_divisor<std::int32_t>},
+            {"int64", &dispatch_divisor<std::int64_t>}
+        };
+
+    auto dispatcher = dispatchers.find(integer_type);
+    if (dispatcher != dispatchers.end())
+    {
+        return dispatcher->second(divisor_type);
+    }
+    else
+    {
+        throw boost::program_options::error(u8"Неверная разрядность сортируемых чисел: " + integer_type);
+    }
+}
+
 int main (int argc, const char * argv[])
 {
     namespace bpo = boost::program_options;
@@ -44,7 +110,13 @@ int main (int argc, const char * argv[])
     description.add_options()
         ("help,h", "Подсказка.")
         ("attempts", bpo::value<std::size_t>()->default_value(1000),
-            "Количество испытаний.");
+            "Количество испытаний.")
+        ("integer", bpo::value<std::string>()->default_value("int32"),
+            "Тип сортируемых чисел.\n"
+            "Допустимые значения: uint8, uint16, uint32, uint64, int8, int16, int32, int64")
+        ("divisor", bpo::value<boost::tuple<std::string, std::int64_t>>()->default_value(boost::make_tuple("int32", 100)),
+            "Тип делителя.\n"
+            "Допустимые значения: uint8, uint16, uint32, uint64, int8, int16, int32, int64");
 
     try
     {
@@ -59,11 +131,11 @@ int main (int argc, const char * argv[])
         else
         {
             auto attempts = vm["attempts"].as<std::size_t>();
+            auto integer_type = vm["integer"].as<std::string>();
+            auto divisor = vm["divisor"].as<boost::tuple<std::string, std::int64_t>>();
 
-            std::vector<std::int32_t> values;
-            read(std::cin, values);
-
-            test_divceil(values, attempts);
+            auto test = get_call(integer_type, boost::get<0>(divisor));
+            test(boost::get<1>(divisor), attempts);
         }
     }
     catch (bpo::error & e)
