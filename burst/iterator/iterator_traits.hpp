33924fad66b04a9beefd266ca6ee9c90f12e1cca
Дмитрий Изволов <dmitriy@izvolov.ru>
2018-02-04 03:11:22 +0300

    refs #81 Общий файл для всех характеристик

---
+++ burst/iterator/iterator_traits.hpp
@@ -0,0 +1,7 @@
+#ifndef BURST_ITERATOR_ITERATOR_TRAITS_HPP
+#define BURST_ITERATOR_ITERATOR_TRAITS_HPP
+
+#include <burst/iterator/iterator_difference.hpp>
+#include <burst/iterator/iterator_value.hpp>
+
+#endif // BURST_ITERATOR_ITERATOR_TRAITS_HPP
