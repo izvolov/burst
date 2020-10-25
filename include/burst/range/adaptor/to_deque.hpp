dce58064b050e45b8df78d0f497dfd2c7f9e2a52
Дмитрий Изволов <dmitriy@izvolov.ru>
2020-10-25 14:52:16 +0300

    refs #139 burst::to_deque

---
+++ include/burst/range/adaptor/to_deque.hpp
@@ -0,0 +1,40 @@
+#ifndef BURST_RANGE_ADAPTOR_TO_DEQUE_HPP
+#define BURST_RANGE_ADAPTOR_TO_DEQUE_HPP
+
+#include <burst/range/adaptor/adaptor.hpp>
+#include <burst/container/make_deque.hpp>
+
+namespace burst
+{
+    struct make_deque_t
+    {
+        template <typename... Xs>
+        constexpr auto operator () (Xs &&... xs) const
+        {
+            return make_deque(std::forward<Xs>(xs)...);
+        }
+    };
+
+    /*!
+        \brief
+            Инструмент для конструирования std::deque через конвейер
+
+        \details
+            Вызов
+
+            \code{.cpp}
+            range | to_deque
+            \endcode
+
+            эквивалентен вызову
+
+            \code{.cpp}
+            make_deque(range)
+            \endcode
+
+        \see make_deque
+     */
+    constexpr auto to_deque = make_adaptor_trigger(make_deque_t{});
+} // namespace burst
+
+#endif // BURST_RANGE_ADAPTOR_TO_DEQUE_HPP
