0221eb1e1fb8a89d466a7a14c2577682505761b6
Дмитрий Изволов <dmitriy@izvolov.ru>
2021-03-21 17:06:09 +0300

    refs #143 Более универсальный подход к песочнице

---
+++ online/burst_example.cpp
@@ -0,0 +1,19 @@
+#include <burst/algorithm/radix_sort.hpp>
+
+#include <cassert>
+#include <string>
+#include <vector>
+
+int main ()
+{
+    std::vector<std::string> strings{"aaaa", "bbb", "cc", "d"};
+
+    std::vector<std::string> buffer(strings.size());
+    burst::radix_sort(strings.begin(), strings.end(), buffer.begin(),
+        [] (const std::string & string)
+        {
+            return string.size(); // Строки сортируются относительно их длины.
+        }
+    );
+    assert((strings == std::vector<std::string>{"d", "cc", "bbb", "aaaa"}));
+}
