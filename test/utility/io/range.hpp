a1d7b96042d070d358baaf62392ba450ac426b8d
Дмитрий Изволов <dmitriy@izvolov.ru>
2021-08-12 09:48:58 +0300

    refs #58 Специализация для печати типа char и ему подобных

--- test/utility/io/range.hpp
+++ test/utility/io/range.hpp
@@ -1,14 +1,18 @@
 #ifndef BURST_TEST_OUTPUT_RANGE_HPP
 #define BURST_TEST_OUTPUT_RANGE_HPP
 
+#include <burst/functional/identity.hpp>
+#include <burst/type_traits/range_value.hpp>
+
 #include <algorithm>
-#include <ostream>
 #include <iterator>
+#include <ostream>
+#include <type_traits>
 
 namespace test_detail
 {
-    template <typename Range>
-    std::ostream & print_range (std::ostream & stream, const Range & range)
+    template <typename Range, typename UnaryFunction>
+    std::ostream & print_range (std::ostream & stream, const Range & range, UnaryFunction proj)
     {
         stream << "[";
 
@@ -17,15 +21,42 @@ namespace test_detail
 
         if (begin(range) != end(range))
         {
-            stream << *begin(range);
+            stream << proj(*begin(range));
             std::for_each(std::next(begin(range)), end(range),
-                [& stream] (const auto & value)
+                [& stream, & proj] (const auto & value)
                 {
-                    stream << ", " << value;
+                    stream << ", " << proj(value);
                 });
         }
 
         return stream << "]";
+    }
+
+    template <typename Range>
+    auto print_range (std::ostream & stream, const Range & range)
+        ->
+            typename std::enable_if
+            <
+                std::is_integral<burst::range_value_t<Range>>::value &&
+                sizeof(burst::range_value_t<Range>) == 1,
+                std::ostream &
+            >
+            ::type
+    {
+        return print_range(stream, range, [] (auto x) -> int {return x;});
+    }
+
+    template <typename Range>
+    auto print_range (std::ostream & stream, const Range & range)
+        ->
+            typename std::enable_if
+            <
+                sizeof(burst::range_value_t<Range>) != 1,
+                std::ostream &
+            >
+            ::type
+    {
+        return print_range(stream, range, burst::identity);
     }
 }
 
