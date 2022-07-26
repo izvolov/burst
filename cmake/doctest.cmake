3d56ce97c1fe0afc2a0b971e28af4caaacf250d8
Дмитрий Изволов <dmitriy@izvolov.ru>
2022-07-26 23:08:46 +0300

    refs #171 Отдельный модуль для doctest

---
+++ cmake/doctest.cmake
@@ -0,0 +1,20 @@
+set(BURST_DOCTEST_VERSION 2.3.4)
+set(BURST_DOCTEST_REPOSITORY https://github.com/doctest/doctest.git)
+
+find_package(doctest ${BURST_DOCTEST_VERSION})
+
+if (doctest_FOUND)
+    message(STATUS "Найден doctest ${doctest_VERSION}: ${doctest_DIR}")
+else()
+    message(STATUS
+        "doctest ${BURST_DOCTEST_VERSION} будет взят с гитхаба: ${BURST_DOCTEST_REPOSITORY}")
+
+    include(FetchContent)
+    FetchContent_Declare(doctest
+        GIT_REPOSITORY
+            ${BURST_DOCTEST_REPOSITORY}
+        GIT_TAG
+            ${BURST_DOCTEST_VERSION}
+    )
+    FetchContent_MakeAvailable(doctest)
+endif()
