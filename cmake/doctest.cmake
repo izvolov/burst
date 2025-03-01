daded0ede36d9ff6688d229b0019dfe7ab008ec6
Дмитрий Изволов <dmitriy@izvolov.ru>
2025-03-01 17:23:11 +0300

    refs #198 Чтобы не было предупреждения, нужна минимум версия 3.5

--- cmake/doctest.cmake
+++ cmake/doctest.cmake
@@ -9,12 +9,18 @@ else()
     message(STATUS
         "doctest ${BURST_DOCTEST_VERSION} будет взят с гитхаба: ${BURST_DOCTEST_REPOSITORY}")
 
+    find_package(Git REQUIRED)
+
     include(FetchContent)
     FetchContent_Declare(doctest
         GIT_REPOSITORY
             ${BURST_DOCTEST_REPOSITORY}
         GIT_TAG
             v${BURST_DOCTEST_VERSION}
+        PATCH_COMMAND
+            # При каждом перепрогоне cmake будет пытаться заново применить патч,
+            # поэтому, если патч применить не удалось, считаем, что он уже применён.
+            ${GIT_EXECUTABLE} apply ${CMAKE_SOURCE_DIR}/cmake/doctest.patch || true
     )
     FetchContent_MakeAvailable(doctest)
 endif()
