9c388ac533c841519913c215d624aa81322a27b0
Дмитрий Изволов <dmitriy@izvolov.ru>
2025-03-03 23:12:24 +0300

    refs #190 ОБласть действия сужена до арифметических типов
    Для перечислений может быть определено пользовательское сравнение, поэтому для них нельзя использовать приведение к целым;
    Указатель не может быть приведён к целочисленному представлению в рамках `constexpr`-выражения.
    Кроме того, числа с плавающей запятой могут не соответствовать стандарту IEEE 754, поэтому для тех чисел, которые ему не
    соответствуют, данное преобразование может быть неприменимо.

--- include/burst/integer/to_ordered_integral.hpp
+++ include/burst/integer/to_ordered_integral.hpp
@@ -3,26 +3,34 @@
 
 #include <burst/integer/detail/to_ordered_integral.hpp>
 
+#include <type_traits>
+#include <limits>
+
 namespace burst
 {
     /*!
         \brief
-            Представить скалярный тип в виде упорядоченного целого
+            Представить арифметический тип в виде упорядоченного целого
 
         \details
-            Функция определена для упорядоченных скалярных типов, то есть для целых чисел,
-            чисел с плавающей запятой, указателей и перечислений.
+            Функция определена для упорядоченных арифметических типов, то есть для целых чисел и
+            чисел с плавающей запятой.
 
             Возвращает такое целочисленное представление, что:
                 ∀ x, y: x < y => to_ordered_integral(x) < to_ordered_integral(y),
-            где `x`, `y` — значения типа `Scalar`.
+            где `x`, `y` — значения типа `Arithmetic`.
      */
     struct to_ordered_integral_fn
     {
-        template <typename Scalar>
-        constexpr auto operator () (Scalar value) const
+        template <typename Arithmetic>
+        constexpr auto operator () (Arithmetic value) const
         {
-            static_assert(std::is_scalar<Scalar>::value, "Значение должно быть скаляром");
+            static_assert
+            (
+                std::is_integral<Arithmetic>::value || std::numeric_limits<Arithmetic>::is_iec559,
+                "Требуется целое число или число с плавающей запятой, "
+                "соответствующее стандарту IEC 559 (IEEE 754)"
+            );
             return detail::to_ordered_integral_impl(value);
         }
     };
