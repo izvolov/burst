d30d34de3d5ff9091ce158951caa8af4571c47e6
Дмитрий Изволов <dmitriy@izvolov.ru>
2025-03-29 13:36:00 +0300

    refs #190 long double также исключён

--- include/burst/integer/detail/to_ordered_integral.hpp
+++ include/burst/integer/detail/to_ordered_integral.hpp
@@ -63,6 +63,36 @@ namespace burst
 
             return static_cast<integral_type>((u & sign_bit_mask) ? ~u : u ^ sign_bit_mask);
         }
+
+        /*!
+            \brief
+                Перегрузка для `long double` запрещена
+
+            \details
+                `long double` сильно различается в зависимости от платформы и компилятора,
+                и определить его битность, чтобы привести к упорядоченному целому, практически
+                невозможно.
+
+                `numeric_limits<long double>::is_iec559` может быть истинно не только для
+                128-битных `long double`, но и для 80-битных. При этом фактический размер
+                80-битного `long double` (то, что возвращает `sizeof`) может быть и 10, и 12, и 16.
+                "Пустые биты", т.е. сверх используемых 80-ти, могут быть заполнены произвольными
+                данными.
+                Определить реальную битность можно только эвристически, например, найдя знаковый бит
+                по формуле:
+
+                \code{.cpp}
+                std::countr_zero
+                (
+                    std::bitcast<uint128_t>(-std::numeric_limits<F>::infinity()) ^
+                    std::bitcast<uint128_t>( std::numeric_limits<F>::infinity())
+                );
+                \endcode
+
+                Но даже если правильно определить битность, то на текущий момент (29.03.2025)
+                `std::bit_cast<uint128_t>(long double)` не является `constexpr`.
+         */
+        inline constexpr auto to_ordered_integral_impl (long double) = delete;
     } // namespace detail
 } // namespace burst
 
